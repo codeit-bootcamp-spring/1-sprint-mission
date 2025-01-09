d0ce525b974f62027b417f159b453612e92e5f8e
백재우 <9712jw@gmail.com>
2025-01-09 21:26:56 +0900

    feat : create each service file

---
+++ codeit-bootcamp-spring/1-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/jcf/JCFChannelService.java
@@ -0,0 +1,6 @@
+package com.sprint.mission.discodeit.service.jcf;
+
+import com.sprint.mission.discodeit.service.channel.ChannelService;
+
+public class JCFChannelService implements ChannelService {
+}
---
+++ codeit-bootcamp-spring/1-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
@@ -0,0 +1,6 @@
+package com.sprint.mission.discodeit.service.jcf;
+
+import com.sprint.mission.discodeit.service.user.UserService;
+
+public class JCFUserService implements UserService {
+}
