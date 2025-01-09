9740a80299ea449af6d99d5c62941fc2842732db
gunhee <sb04214@naver.com>
2025-01-09 10:11:07 +0900

    Update JavaApplication.java

--- src/main/java/com/sprint/mission/discodeit/JavaApplication.java
+++ src/main/java/com/sprint/mission/discodeit/JavaApplication.java
@@ -17,7 +17,7 @@ public class JavaApplication {
         System.out.println("생성된 사용자: " + userService.read(user.getId()));
 
         // 사용자 수정
-        user.updateUsername("AliceUpdated");
+        user.updateUsername("장건희 updated");
         userService.update(user.getId(), user);
         System.out.println("수정된 사용자: " + userService.read(user.getId()));
 
@@ -30,12 +30,12 @@ public class JavaApplication {
         JCFChannelService channelService = new JCFChannelService();
 
         // 채널 생성
-        Channel channel = new Channel("General", "일반 토론 채널");
+        Channel channel = new Channel("General", "일반 테스트 채널");
         channelService.create(channel);
         System.out.println("생성된 채널: " + channelService.read(channel.getId()));
 
         // 채널 수정
-        channel.updateDescription("업데이트된 일반 토론 채널");
+        channel.updateDescription("업데이트된 테스트 채널");
         channelService.update(channel.getId(), channel);
         System.out.println("수정된 채널: " + channelService.read(channel.getId()));
 
@@ -50,12 +50,12 @@ public class JavaApplication {
         // 메시지 생성
         UUID senderId = UUID.randomUUID(); // 가상의 발신자 ID
         UUID channelId = UUID.randomUUID(); // 가상의 채널 ID
-        Message message = new Message("안녕하세요, 세계!", senderId, channelId);
+        Message message = new Message("안녕하세요, 장건희입니다!", senderId, channelId);
         messageService.create(message);
         System.out.println("생성된 메시지: " + messageService.read(message.getId()));
 
         // 메시지 수정
-        message.updateContent("안녕하세요, 업데이트된 세계!");
+        message.updateContent("안녕하세요, 업데이트된 장건희입니다!");
         messageService.update(message.getId(), message);
         System.out.println("수정된 메시지: " + messageService.read(message.getId()));
 
