9c1f19c8e5e94c1e79a3535fe1088d3aeda99e23
백재우 <9712jw@gmail.com>
2025-01-09 22:52:22 +0900

    test : test userRepository

--- codeit-bootcamp-spring/1-sprint-mission/src/test/java/com/sprint/mission/discodeit/db/user/UserRepositoryImplTest.java
+++ codeit-bootcamp-spring/1-sprint-mission/src/test/java/com/sprint/mission/discodeit/db/user/UserRepositoryImplTest.java
@@ -1,24 +1,39 @@
 package com.sprint.mission.discodeit.db.user;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-import com.sprint.mission.discodeit.db.user.ifs.UserRepository;
+import com.sprint.mission.discodeit.entity.user.User;
+import com.sprint.mission.discodeit.entity.user.UserName;
 import com.sprint.mission.discodeit.testdummy.TestDummyFactory;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class UserRepositoryImplTest {
-
-    private UserRepository userRepository;
+    private static final String TEST_NAME = "Test";
+    private User user;
+    private UserRepositoryImpl userRepository;
 
     @BeforeEach
     void setUp() {
         userRepository = TestDummyFactory.getUserRepository();
+        user = new User(new UserName(TEST_NAME));
+        userRepository.save(user);
     }
 
 
     @Test
+    @DisplayName("사용자 이름으로 찾을 때 유저정보 객체를 리턴")
     void givenUsernameWhenFindByNameThenReturnUser() {
-
+        // given
+        // when
+        var findUser = userRepository.findByUsername(TEST_NAME).orElse(null);
+        // then
+        assertAll(
+                () -> assertThat(findUser).isEqualTo(user),
+                () -> assertThat(findUser).isNotNull()
+        );
     }
+
 }
