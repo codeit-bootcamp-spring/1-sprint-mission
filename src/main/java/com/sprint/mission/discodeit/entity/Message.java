aa6011e41d3dd8657871d31a023cf0e5aa14ae2b
kdi <66400384+kbkg11@users.noreply.github.com>
2025-01-10 16:35:21 +0900

    주석 업데이트

--- src/main/java/com/sprint/mission/discodeit/entity/Message.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Message.java
@@ -18,13 +18,14 @@ public class Message {
         this.senderId = senderId;
         this.channelId = channelId;
     }
-
+    // 각 필드의 Getter 함수 정의
     public UUID getId() { return id; }
-    public long getCreatedAt() { return createdAt; }
-    public long getUpdatedAt() { return updatedAt; }
     public String getContent() { return content; }
+    // 현재는 사용하지 않는 값이지만, 확장성을 위해 모든 필드에 대해 Getter 함수 작성
     public UUID getSenderId() { return senderId; }
     public UUID getChannelId() { return channelId; }
+    public long getCreatedAt() { return createdAt; }
+    public long getUpdatedAt() { return updatedAt; }
 
     public void updateContent(String newContent) {
         this.content = newContent;
--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -17,10 +17,12 @@ public class User {
         this.password = password;
     }
 
+    // 각 필드의 Getter 함수 정의
     public UUID getId() { return id; }
+    public String getUsername() { return username; }
+    // 현재는 사용하지 않는 값이지만, 확장성을 위해 모든 필드에 대해 Getter 함수 작성
     public long getCreatedAt() { return createdAt; }
     public long getUpdatedAt() { return updatedAt; }
-    public String getUsername() { return username; }
     public String getPassword() { return password; }
 
     public void updateUsername(String newUsername) {
