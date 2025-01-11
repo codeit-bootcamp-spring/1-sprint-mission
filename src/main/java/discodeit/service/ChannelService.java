880c126d077aa859e537359de66ce0e9e7a0518b
yxoni <psy337250@gmail.com>
2025-01-11 19:29:19 +0900

    feat: ChannelService 인터페이스 생성

---
+++ src/main/java/discodeit/service/ChannelService.java
@@ -0,0 +1,24 @@
+package discodeit.service;
+
+import discodeit.entity.Channel;
+import discodeit.entity.User;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ChannelService {
+    void createChannel(String name, String introduction, User owner);
+    UUID getId(Channel channel);
+    long getCreatedAt(Channel channel);
+    long getUpdatedAt(Channel channel);
+    void getInfo(Channel channel);
+    String getName(Channel channel);
+    String getIntroduction(Channel channel);
+    User getOwner(Channel channel);
+    List<User> getParticipants(Channel channel);
+    void updateName(Channel channel, String name);
+    void updateIntroduction(Channel channel, String introduction);
+    void updateParticipants(Channel channel, User user);
+    void deleteChannel(Channel channel);
+    void deleteParticipant(Channel channel, User user);
+}
