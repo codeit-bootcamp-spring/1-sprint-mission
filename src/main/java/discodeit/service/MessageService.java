86093939d03e40a307b85b598e84b13b9a95f855
yxoni <psy337250@gmail.com>
2025-01-11 19:34:37 +0900

    feat: MessageService 생성

---
+++ src/main/java/discodeit/service/MessageService.java
@@ -0,0 +1,18 @@
+package discodeit.service;
+
+import discodeit.entity.Message;
+import discodeit.entity.User;
+
+import java.util.UUID;
+
+public interface MessageService {
+    void createMessage(String content, User sender);
+    UUID readId(Message message);
+    long getCreateAt(Message message);
+    long getUpdatedAt(Message message);
+    void getInfo(Message message);
+    String getContent(Message message);
+    User getSender(Message message);
+    void updateContent(Message message);
+    void deleteMessage(Message message);
+}
