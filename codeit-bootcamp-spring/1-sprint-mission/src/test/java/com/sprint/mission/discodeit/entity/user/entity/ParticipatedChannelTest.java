19d4016775f3eeede13f1125eb5500944f97914d
백재우 <9712jw@gmail.com>
2025-01-11 19:20:25 +0900

    test : test code participatedChannel in User

---
+++ codeit-bootcamp-spring/1-sprint-mission/src/test/java/com/sprint/mission/discodeit/entity/user/entity/ParticipatedChannelTest.java
@@ -0,0 +1,54 @@
+package com.sprint.mission.discodeit.entity.user.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class ParticipatedChannelTest {
+    private static final String CHANNEL_NAME = "코드잇-1기-백엔드";
+    private ParticipatedChannel participatedChannel;
+
+    @Test
+    void givenNewParticipatedChannelWhenCreateThenReturnNewParticipatedChannel() {
+        // given when
+        var createdParticipatedChannel = ParticipatedChannel.from(new ArrayList<>());
+        // then
+        assertThat(createdParticipatedChannel).isNotNull();
+    }
+    // TODO  생성 테스트
+
+    @Nested
+    @DisplayName("채널 crud")
+    class CrudParticipatedChannelTest {
+
+        @BeforeEach
+        void setUp() {
+            participatedChannel = ParticipatedChannel.from(new ArrayList<>());
+        }
+
+        @Test
+        void givenChannelNameWhenCreateChannelThenReturnNewChannel() {
+            // given when
+            var channel = participatedChannel.createChannel(CHANNEL_NAME);
+            // then
+            assertAll(
+                    () -> {
+                        assertThat(channel).isNotNull();
+
+                        assertAll(
+                                () -> assertThat(channel.getChannelName()).isEqualTo(CHANNEL_NAME)
+                        );
+                    }
+            );
+        }
+        // TODO 채널 찾기 테스트
+
+        // TODO 채널 수정 테스트
+    }
+
+}
