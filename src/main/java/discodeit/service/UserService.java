668284cb35610ca7f7ef4d8c2f4c7cc428bcaf09
yxoni <psy337250@gmail.com>
2025-01-11 18:46:27 +0900

    feat: UserService 인터페이스 생성

---
+++ src/main/java/discodeit/service/UserService.java
@@ -0,0 +1,26 @@
+package discodeit.service;
+
+import discodeit.entity.Channel;
+import discodeit.entity.User;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface UserService {
+    void createUser(String name, String email, String phoneNumber, String password);
+    UUID readId(User user);
+    long getCreatedAt(User user);
+    long getUpdatedAt(User user);
+    void getInfo(User user);
+    String getName(User user);
+    String getEmail(User user);
+    String getPhoneNumber(User user);
+    List<Channel> getJoinedChannels(User user);
+    void updateName(User user, String name);
+    void updateEmail(User user, String email);
+    void updatePhoneNumber(User user, String phoneNumber);
+    void updatePassword(User user, String password);
+    void updateJoinedChannels(User user, Channel channel);
+    void deleteUser(User user);
+    void deleteJoinedChannel(User user, Channel channel);
+}
