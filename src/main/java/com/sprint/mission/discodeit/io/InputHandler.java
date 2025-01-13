50a26a69e944ee6cdd4e1206c18a79517f27bd17
kyungrin Kim <0123jade0123@gmail.com>
2025-01-13 17:41:04 +0900

    Refactor: 입출력 로직을 io 패키지로 분리

--- src/main/java/com/sprint/mission/discodeit/io/InputHandler.java
+++ src/main/java/com/sprint/mission/discodeit/io/InputHandler.java
@@ -1,21 +1,19 @@
 package com.sprint.mission.discodeit.io;
 
-import org.springframework.stereotype.Component;
-
 import java.util.Scanner;
 
-@Component
 public class InputHandler {
     private Scanner sc;
     public InputHandler(){
         sc = new Scanner(System.in);
     }
-    public String getNewInput(){
-        System.out.println("update :");
+    public String getNewNickNameInput(){
+        System.out.println("new Nickname :");
         return sc.next();
     }
     public String getYesNOInput(){
-        System.out.println("Y/N \n");
+        System.out.println("Are you sure you want to delete it?\n");
         return sc.next();
     }
+
 }
