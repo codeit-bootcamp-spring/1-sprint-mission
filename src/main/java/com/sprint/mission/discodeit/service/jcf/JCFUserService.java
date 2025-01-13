32e4dba1fe94d33757ecec347ceda3a3b156b1f1
kyungrin Kim <0123jade0123@gmail.com>
2025-01-13 17:46:13 +0900

    Test: ãJCFUserService 단위 테스트 추가

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
@@ -2,16 +2,26 @@ package com.sprint.mission.discodeit.service.jcf;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.service.UserService;
 import java.util.HashMap;
-import java.util.Scanner;
+import com.sprint.mission.discodeit.io.InputHandler;
 
 
 public class JCFUserService implements UserService {
-    Scanner sc = new Scanner(System.in);
+    //Scanner sc = new Scanner(System.in);
     // HashMap(nickname - user)
     private final HashMap<String, User> Users;
-    public JCFUserService(){
+    // mocking 이용으로 추가
+    private InputHandler inputHandler;
+
+    public JCFUserService(InputHandler inputHandler){
         // 생성자에서 users 데이터 초기화
         this.Users = new HashMap<>();
+        // mocking 이용으로 추가
+        this.inputHandler = inputHandler;
+    }
+
+    // mocking 이용으로 추가
+    public void setInputHandler(InputHandler inputHandler) {
+        this.inputHandler = inputHandler;
     }
 
     // 의존성 주입을 위한 user 반환
@@ -24,28 +34,30 @@ public class JCFUserService implements UserService {
         Users.put(nickname, user);
     }
 
-    public void ReadAll(){
+    public int ReadAll(){
         // 전체 유저 조희
         if (Users.isEmpty()) {
             System.out.println("No users exists.\n");
         }else{
             System.out.println("All Users : " + Users.keySet());
         }
+        return Users.size();
     }
-    public void ReadUser(String nickname){
+    public String ReadUser(String nickname){
         if(Users.get(nickname) == null ){
             System.out.println(nickname + " does not exist\n");
         }else{
             System.out.println("User name is " + Users.get(nickname).getNickname());
             System.out.println("You created this account at: " + Users.get(nickname).getCreatedAt());
         }
+        return Users.get(nickname).getNickname();
     }
 
     public void UpdateNickname(String nickname){
         // nickname 을 key로 설정했어서
         // delete 과정을 거친 후 생성
-        System.out.println("new Nickname :");
-        String newNickname = sc.next();
+        //String newNickname = sc.next();
+        String newNickname = inputHandler.getNewNickNameInput();
         Users.put(newNickname, Users.get(nickname));
         Users.get(newNickname).setNickname(newNickname);
         Users.remove(nickname);
@@ -54,17 +66,15 @@ public class JCFUserService implements UserService {
     }
 
     public void DeleteAll(){
-        System.out.println("Do you really delete everything?\n");
-        System.out.println("                 [y/n]");
-        String keyword = sc.next().toLowerCase();
+        //String keyword = sc.next().toLowerCase();
+        String keyword = inputHandler.getYesNOInput().toLowerCase();
         if(keyword.equals("y")){
             Users.clear();
         }
     }
     public void DeleteUser(String nickname){
-        System.out.println("Do you really delete user?\n");
-        System.out.println("                 [y/n]");
-        String keyword = sc.next().toLowerCase();
+        //String keyword = sc.next().toLowerCase();
+        String keyword = inputHandler.getYesNOInput().toLowerCase();
         if(keyword.equals("y")){
             Users.remove(nickname);
         }
