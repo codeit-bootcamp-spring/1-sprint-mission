ecabf44e71f657f238941e9194d8ad6b796369af
kyungrin Kim <0123jade0123@gmail.com>
2025-01-14 17:28:18 +0900

    refactor : ìã 서비스 로직의 인터페이스에서 추ìabstract 생략

--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -5,17 +5,16 @@ import com.sprint.mission.discodeit.entity.User;
 import java.util.UUID;
 
 public interface UserService {
-    public abstract UUID createUser(String nickname);
-
+    UUID createUser(String nickname);
     // Read : 전체 유저 조회, 특정 유저 조회
-    public abstract int showAllUsers();
-    public abstract User getUserById(UUID id);
+    int showAllUsers();
+    User getUserById(UUID id);
 
     // Update : 특정 유저 닉네임 변경
-    public abstract void updateUserNickname(UUID id);
+    void updateUserNickname(UUID id);
 
     // Delete : 전체 유저 삭제, 특정 유저 삭제
-    public abstract void clearAllUsers();
-    public abstract void removeUserById(UUID id);
+    void clearAllUsers();
+    void removeUserById(UUID id);
 
 }
