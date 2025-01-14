f5e316e8615f057cda046acb6ba1680ee2ad40e2
kyungrin Kim <0123jade0123@gmail.com>
2025-01-14 10:24:41 +0900

    refactor : Service layer상에 존재하던 데이터 처리 분리를 위한 repository 패키지 생성

--- src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -1,17 +1,4 @@
 package com.sprint.mission.discodeit.repository;
 
-import com.sprint.mission.discodeit.entity.Message;
-
-import java.util.List;
-import java.util.UUID;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, UUID> {
-
-  List<Message> findByChannelId(UUID channelId); // Channel 에 포함된 메세지 찾기
-
-  Page<Message> findByChannelId(UUID channelId, Pageable pageable);
+public interface MessageRepository {
 }
-
--- src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -1,18 +1,4 @@
 package com.sprint.mission.discodeit.repository;
 
-import com.sprint.mission.discodeit.entity.User;
-
-import java.util.Optional;
-import java.util.UUID;
-import javax.swing.text.html.Option;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, UUID> {
-
-  // 검증을 위한 쿼리 메서드 추가 -> 모든 User 객체를 불러와 비교하는 방식은 비효율적이기 때문에
-  boolean existsByUsername(String username);
-
-  boolean existsByEmail(String email);
-
-  Optional<User> findByUsername(String username);
+public interface UserRepository {
 }
