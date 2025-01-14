93eeb494049a5a71fa713c2da757d139b4cb738f
yxoni <psy337250@gmail.com>
2025-01-15 01:21:24 +0900

    test: 유저 중복 이메일, 전화번호 테스트 작성

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,4 +1,7 @@
 import discodeit.entity.User;
+import discodeit.service.ServiceFactory;
+import discodeit.service.UserService;
+import discodeit.service.jcf.JCFServiceFactory;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -21,5 +24,31 @@ public class UserTest {
         assertThatThrownBy(() -> user.updatePassword("123", "123"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("잘못된 비밀번호입니다.");
+    }
+
+    @Test
+    void 중복_이메일_설정_시_오류() {
+        ServiceFactory serviceFactory = JCFServiceFactory.getInstance();
+        UserService jcfUserService = serviceFactory.getUserService();
+
+        User user1 = jcfUserService.createUser("name", "email1@codeit.com", "010-1111-1111", "1234");
+        User user2 = jcfUserService.createUser("name", "email2@codeit.com", "010-2222-2222", "1234");
+
+        assertThatThrownBy(() -> jcfUserService.updateEmail(user2, "email1@codeit.com"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이미 존재하는 이메일입니다.");
+    }
+
+    @Test
+    void 중복_전화번호_설정_시_오류() {
+        ServiceFactory serviceFactory = JCFServiceFactory.getInstance();
+        UserService jcfUserService = serviceFactory.getUserService();
+
+        User user1 = jcfUserService.createUser("name", "email3@codeit.com", "010-3333-3333", "1234");
+        User user2 = jcfUserService.createUser("name", "email4@codeit.com", "010-4444-4444", "1234");
+
+        assertThatThrownBy(() -> jcfUserService.updatePhoneNumber(user2, "010-3333-3333"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이미 존재하는 번호입니다.");
     }
 }
