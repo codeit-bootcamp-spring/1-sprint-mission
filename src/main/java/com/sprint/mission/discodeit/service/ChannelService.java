dbd4ddbdfcf1d4d92ccf2e713895b2a171ad6d3a
kyungrin Kim <0123jade0123@gmail.com>
2025-01-14 17:28:18 +0900

    refactor : ìã 서비스 로직의 인터페이스에서 추ìabstract 생략

--- src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
@@ -5,16 +5,16 @@ import com.sprint.mission.discodeit.entity.User;
 import java.util.UUID;
 
 public interface ChannelService {
-    public abstract UUID createChannel(User user, String channelName);
+    UUID createChannel(User user, String channelName);
 
     // Read : 전체 채널 조회, 특정 채널 조회
-    public abstract int showAllChannels();
-    public abstract Channel getChannelById(UUID id);
+    int showAllChannels();
+    Channel getChannelById(UUID id);
 
     // Update : 특정 채널 이름 변경
-    public abstract void updateChannelName(UUID id);
+    void updateChannelName(UUID id);
 
     // Delete : 전체 채널 삭제, 특정 채널 삭제
-    public abstract void deleteAllChannels();
-    public abstract void deleteChannelById(UUID id);
+    void deleteAllChannels();
+    void deleteChannelById(UUID id);
 }
--- src/main/java/com/sprint/mission/discodeit/service/MessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/MessageService.java
@@ -5,16 +5,16 @@ import com.sprint.mission.discodeit.entity.Message;
 import java.util.UUID;
 
 public interface MessageService {
-    public abstract UUID createMessage(Channel channel, String messageText);
+    UUID createMessage(Channel channel, String messageText);
 
     // Read : 전체 메세지 조회, 특정 메세지 읽기
-    public abstract int showAllMessages();
-    public abstract Message getMessageById(UUID id);
+    int showAllMessages();
+    Message getMessageById(UUID id);
 
     // Update : 특정 메세지 수정
-    public abstract void updateMessageText(UUID id);
+    void updateMessageText(UUID id);
 
     // Delete : 전체 메세지 삭제, 특정 메세지 삭제
-    public abstract void deleteAllMessages();
-    public abstract void deleteMessageById(UUID id);
+    void deleteAllMessages();
+    void deleteMessageById(UUID id);
 }
