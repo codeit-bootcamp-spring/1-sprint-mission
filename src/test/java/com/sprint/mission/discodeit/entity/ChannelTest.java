dc448a546d05198779b9927b43b4b0412d21c6e9
you12 <tjf7894@gmail.com>
2025-01-15 16:34:50 +0900

    SLF4J and Logback add

---
+++ src/test/java/com/sprint/mission/discodeit/entity/ChannelTest.java
@@ -0,0 +1,42 @@
+package com.sprint.mission.discodeit.entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class ChannelTest {
+
+    private static final Logger log = LoggerFactory.getLogger(ChannelTest.class);
+
+
+    @BeforeEach
+    void testStart(){
+        log.info("=========================testStart===========================");
+    }
+
+    @Test
+    void getuuId() {
+    }
+
+    @Test
+    void getCreatedAt() {
+    }
+
+    @Test
+    void getUpdatedAt() {
+    }
+
+    @Test
+    void getChannelName() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
