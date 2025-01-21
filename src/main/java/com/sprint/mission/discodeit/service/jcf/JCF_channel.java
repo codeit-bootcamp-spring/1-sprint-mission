d9e53d338d3c9d7cc150a54880adf2e288dbd894
박종우 <101695894+wchorong@users.noreply.github.com>
2025-01-21 13:39:56 +0900

    Update src/main/java/com/sprint/mission/discodeit/service/jcf/JCF_channel.java
    Co-authored-by: Yeongun Heo <52141636+yeongunheo@users.noreply.github.com>

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCF_channel.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCF_channel.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 public class JCF_channel implements ChannelService {
-    private final List<Channel> channelSet;
+    private final List<Channel> channelList;
 
     public JCF_channel() {
         this.channelSet = new ArrayList<>();
