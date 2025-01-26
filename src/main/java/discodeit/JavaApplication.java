54e07d3251b2daaa69c789a52899248d009022ab
yxoni <psy337250@gmail.com>
2025-01-26 17:28:37 +0900

    feat: Application에서 File*Service로 테스트

--- src/main/java/discodeit/JavaApplication.java
+++ src/main/java/discodeit/JavaApplication.java
@@ -5,76 +5,92 @@ import discodeit.entity.Message;
 import discodeit.entity.User;
 import discodeit.service.ChannelService;
 import discodeit.service.MessageService;
-import discodeit.service.ServiceFactory;
 import discodeit.service.UserService;
-import discodeit.service.jcf.JCFServiceFactory;
+import discodeit.service.file.FileChannelService;
+import discodeit.service.file.FileMessageService;
+import discodeit.service.file.FileUserService;
+
+import java.util.List;
 
 public class JavaApplication {
+
+    static void userCRUDTest(UserService userService) {
+        // 생성
+        User user = userService.create("user", "user@codeit.com", "010-1111-1111", "qwer1234");
+        System.out.println("유저 생성: " + user.getId());
+        // 조회
+        User foundUser = userService.find(user.getId());
+        System.out.println("유저 조회(단건): " + userService.getInfo(foundUser.getId()));
+        List<User> foundUsers = userService.findAll();
+        System.out.println("유저 조회(다건): " + foundUsers.size());
+        // 수정
+        userService.update(user.getId(), "user", "user@codeit.com", "010-1111-1111");
+        System.out.println("유저 수정: " + userService.getInfo(user.getId()));
+        // 삭제
+        userService.delete(user.getId());
+        List<User> foundUsersAfterDelete = userService.findAll();
+        System.out.println("유저 삭제: " + foundUsersAfterDelete.size());
+    }
+
+    static void channelCRUDTest(ChannelService channelService, User owner) {
+        // 생성
+        Channel channel = channelService.create("공지", "공지 채널입니다.", owner);
+        System.out.println("채널 생성: " + channel.getId());
+        // 조회
+        Channel foundChannel = channelService.find(channel.getId());
+        System.out.println("채널 조회(단건): " + channelService.getInfo(foundChannel.getId()));
+        List<Channel> foundChannels = channelService.findAll();
+        System.out.println("채널 조회(다건): " + foundChannels.size());
+        // 수정
+        channelService.update(channel.getId(), "공지", "공지입니다");
+        System.out.println("채널 수정: " + channelService.getInfo(channel.getId()));
+        // 삭제
+        channelService.delete(channel.getId());
+        List<Channel> foundChannelsAfterDelete = channelService.findAll();
+        System.out.println("채널 삭제: " + foundChannelsAfterDelete.size());
+    }
+
+    static void messageCRUDTest(MessageService messageService, Channel channel, User author) {
+        // 생성
+        Message message = messageService.create("안녕하세요.", author, channel.getId());
+        System.out.println("메시지 생성: " + message.getId());
+        // 조회
+        Message foundMessage = messageService.find(message.getId());
+        System.out.println("메시지 조회(단건): " + messageService.getInfo(foundMessage.getId()));
+        List<Message> foundMessages = messageService.findAll();
+        System.out.println("메시지 조회(다건): " + foundMessages.size());
+        // 수정
+        messageService.update(message.getId(), "반갑습니다.");
+        System.out.println("메시지 수정: " + messageService.getInfo(message.getId()));
+        // 삭재
+        messageService.delete(message.getId());
+        List<Message> foundMessagesAfterDelete = messageService.findAll();
+        System.out.println("메시지 삭제: " + foundMessagesAfterDelete.size());
+    }
+
+    static User setupUser(UserService userService) {
+        User user = userService.create("user", "user@codeit.com", "010-1111-1111", "qwer1234");
+        return user;
+    }
+
+    static Channel setupChannel(ChannelService channelService, User owner) {
+        Channel channel = channelService.create("공지", "공지 채널입니다.", owner);
+        return channel;
+    }
+
     public static void main(String[] args) {
-/*
-        // 서비스 객체 초기화
-        ServiceFactory serviceFactory = JCFServiceFactory.getInstance();
-        UserService jcfUserService = serviceFactory.getUserService();
-        ChannelService jcfChannelService = serviceFactory.getChannelService();
-        MessageService jcfMessageService = serviceFactory.getMessageService();
 
-        // 유저 등록 후 조회
-        User user1 = jcfUserService.createUser("user1", "user1@codeit.com", "010-1234-5678", "qwer1234");
-        User user2 = jcfUserService.createUser("user2", "user2@codeit.com", "010-5678-1234", "qwer1234");
-        User user3 = jcfUserService.createUser("user3", "user3@codeit.com", "010-4321-8765", "qwer1234");
+        UserService userService = new FileUserService();
+        ChannelService channelService = new FileChannelService();
+        MessageService messageService = new FileMessageService(userService, channelService);
 
-        System.out.println("***** 유저1, 2, 3 조회 *****");
-        System.out.println(jcfUserService.getInfo(user1) + System.lineSeparator());
-        System.out.println(jcfUserService.getInfo(user2) + System.lineSeparator());
-        System.out.println(jcfUserService.getInfo(user3) + System.lineSeparator());
+        // 셋업
+        User user = setupUser(userService);
+        Channel channel = setupChannel(channelService, user);
 
-        // 유저 정보 수정 후 조회
-//        jcfUserService.updateName(user3, "유저3");
-
-        System.out.println("***** 유저3 이름 수정 *****");
-        System.out.println(jcfUserService.getInfo(user3) + System.lineSeparator());
-
-        // 유저1이 채널 등록
-        Channel channel1 = jcfChannelService.createChannel("channel1", "user1의 채널입니다.", user1);
-
-        // 유저2, 유저3 채널 가입
-        jcfChannelService.updateParticipants(channel1, user2);
-        jcfChannelService.updateParticipants(channel1, user3);
-
-        // 채널 조회
-        System.out.println("***** 유저1의 채널에 유저2, 3이 가입 *****");
-        System.out.println(jcfChannelService.getInfo(channel1) + System.lineSeparator());
-
-        // 채널에 메시지 보낸 후 채널 조회
-        Message message1 = jcfMessageService.createMessage(channel1, "안녕하세요~", user1);
-        Message message2 = jcfMessageService.createMessage(channel1, "방장입니다!", user1);
-        Message message3 = jcfMessageService.createMessage(channel1, "user2라고 합니다.", user2);
-        Message message4 = jcfMessageService.createMessage(channel1, "잘 부탁하빈다. ^^", user3);
-
-        System.out.println("***** 채널에 메시지 보낸 후 조회 *****");
-        System.out.println(jcfChannelService.getInfo(channel1) + System.lineSeparator());
-
-        // 메시지 수정, 삭제 후 채널 조회
-        jcfMessageService.deleteMessage(message2, channel1, user1);
-        jcfMessageService.updateContent(message4, "잘 부탁합니다. ^^");
-
-        System.out.println("***** 유저1 메시지 삭제, 유저3 메시지 수정 *****");
-        System.out.println(jcfChannelService.getInfo(channel1) + System.lineSeparator());
-
-        // 유저 삭제 후 채널 조회 (참가자 목록에서 제거 되었는지 확인)
-        // 채널 정보 수정
-        jcfUserService.deleteUser(user2);
-//        jcfChannelService.updateIntroduction(channel1, "채널 설명 수정했습니다.", user1);
-
-        System.out.println("***** 유저2 회원 탈퇴, 채널 소개 수정 *****");
-        System.out.println(jcfChannelService.getInfo(channel1) + System.lineSeparator());
-
-        // 채널 삭제
-        jcfChannelService.deleteChannel(channel1, user1);
-
-        System.out.println("***** 채널 삭제 후 유저들의 참여 채널 목록에서 정상 삭제 여부 조회 *****");
-        System.out.println(jcfUserService.getInfo(user1) + System.lineSeparator());
-        System.out.println(jcfUserService.getInfo(user3));
-        */
+        // 테스트
+        userCRUDTest(userService);
+        channelCRUDTest(channelService, user);
+        messageCRUDTest(messageService, channel, user);
     }
 }
