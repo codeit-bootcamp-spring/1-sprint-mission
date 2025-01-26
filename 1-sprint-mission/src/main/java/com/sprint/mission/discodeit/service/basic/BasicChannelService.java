fbe19dd124cff68d57e502708d6f8576ca2a9e0e
kbh <xjvm7001@naver.com>
2025-01-26 21:08:20 +0900

    Basic*Service 기본 구현

--- 1-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
+++ 1-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
@@ -1,4 +1,44 @@
 package com.sprint.mission.discodeit.service.basic;
 
-public class BasicChannelService {
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.service.ChannelService;
+
+import java.util.List;
+
+public class BasicChannelService implements ChannelService {
+    private final ChannelRepository channelRepository;
+
+    public BasicChannelService(ChannelRepository channelRepository) {
+        this.channelRepository = channelRepository;
+    }
+
+    @Override
+    public void createChannel(Channel channel) {
+        channelRepository.save(channel);
+        System.out.println("Channel created: " + channel.getChannelTitle() + " (UUID: " + channel.getChannelUuid() + ")");
+    }
+
+    @Override
+    public Channel readChannel(String channelUuid) {
+        return channelRepository.findByUuid(channelUuid);
+    }
+
+    @Override
+    public List<Channel> readAllChannels() {
+        return channelRepository.findAll();
+    }
+
+    @Override
+    public void updateChannel(String channelUuid, String newChannelTitle) {
+        channelRepository.delete(channelUuid);
+        channelRepository.save(channelRepository.findByUuid(channelUuid));
+        System.out.println("Channel updated: " + channelRepository.findByUuid(channelUuid).getChannelTitle() + " (UUID: " + channelUuid + ")");
+    }
+
+    @Override
+    public void deleteChannel(String channelUuid) {
+        channelRepository.delete(channelUuid);
+        System.out.println("Channel deleted: UUID " + channelUuid);
+    }
 }
--- 1-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
+++ 1-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
@@ -1,4 +1,44 @@
 package com.sprint.mission.discodeit.service.basic;
 
-public class BasicMessageService {
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.service.MessageService;
+
+import java.util.List;
+
+public class BasicMessageService implements MessageService {
+    private final MessageRepository messageRepository;
+
+    public BasicMessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    @Override
+    public void createMessage(Message message) {
+        messageRepository.save(message);
+        System.out.println("Message created: " + message.getMessageText() + " (UUID: " + message.getMessageUuid() + ")");
+    }
+
+    @Override
+    public Message readMessage(String messageUuid) {
+        return messageRepository.findByUuid(messageUuid);
+    }
+
+    @Override
+    public List<Message> readAllMessages() {
+        return messageRepository.findAll();
+    }
+
+    @Override
+    public void updateMessage(String messageUuid, String newMessageText) {
+        messageRepository.delete(messageUuid);
+        messageRepository.save(new Message(messageUuid, newMessageText));
+        System.out.println("Message updated: " + newMessageText + " (UUID: " + messageUuid + ")");
+    }
+
+    @Override
+    public void deleteMessage(String messageUuid) {
+        messageRepository.delete(messageUuid);
+        System.out.println("Message deleted: UUID " + messageUuid);
+    }
 }
