08700a5cdbfb7d47a1b7ef1aabf2744ef19f247f
JunyungLee <skyjn011@gmail.com>
2025-01-29 18:59:03 +0900

    Basic*Service

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
@@ -1,4 +1,86 @@
 package com.sprint.mission.discodeit.service.basic;
 
-public class BasicChannelService {
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.service.ChannelService;
+import com.sprint.mission.discodeit.service.MessageService;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class BasicChannelService implements ChannelService {
+    private ChannelRepository channelRepository;
+    private UserService userService;
+    private MessageService messageService;
+
+    public BasicChannelService(ChannelRepository channelRepository) {
+        this.channelRepository = channelRepository;
+    }
+    public void setService(UserService userService, MessageService messageService) {
+        this.userService = userService;
+        this.messageService = messageService;
+    }
+    @Override
+    public Channel createChannel(String title, User owner) {
+       if(correctTitle(title)) {
+           Channel newChannel = new Channel(title, owner);
+           channelRepository.save(newChannel);
+           System.out.println(owner.getUserName() + "님께서 새로운 채널을 생성했습니다.");
+           return newChannel;
+       }
+        return null;
+    }
+
+    @Override
+    public List<Channel> getAllChannelList() {
+        return channelRepository.findAll().values().stream().collect(Collectors.toList());
+    }
+
+    @Override
+    public Channel searchById(UUID channelId) {
+        Channel channel = channelRepository.findById(channelId);
+        if(channel == null) {
+            System.out.println("해당 채널이 없습니다.");
+        }
+        return channel;
+    }
+
+    @Override
+    public void updateChannelName(UUID channelId, String newChannelName) {
+        try {
+            Channel channel = searchById(channelId);
+            String channelName = channel.getChannelName();
+            channel.setChannelName(newChannelName);
+            channelRepository.save(channel);
+            System.out.println(channelName + "의 채널 이름이" + newChannelName + "으로 변경되었습니다.");
+        } catch (NoSuchElementException e) {
+            System.out.println("해당 채널이 존재하지 않습니다.");
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void deleteChannel(UUID channelId) {
+        Channel channel = searchById(channelId);
+        if(channel == null) {
+            System.out.println("해당 채널이 존재하지 않습니다.");
+        } else  {
+            channelRepository.delete(channelId);
+            System.out.println("해당 채널이 삭제되었습니다.");
+        }
+
+    }
+
+    private boolean correctTitle(String channelName) {
+        if (channelName.isBlank()) {
+            System.out.println("제목을 입력해주세요");
+            return false;
+        }
+        return true;
+    }
 }
--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
@@ -1,4 +1,83 @@
 package com.sprint.mission.discodeit.service.basic;
 
-public class BasicMessageService {
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.service.ChannelService;
+import com.sprint.mission.discodeit.service.MessageService;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class BasicMessageService implements MessageService {
+    private MessageRepository messageRepository;
+    private UserService userService;
+    private ChannelService channelService;
+
+    public BasicMessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+    public void setService(UserService userService, ChannelService channelService) {
+        this.userService = userService;
+        this.channelService = channelService;
+    }
+    @Override
+    public Message createMessage(UUID channelId, UUID authorId, String content) {
+        if(isContent(content)) {
+            Message newMessage = new Message(content, channelId, authorId);
+            messageRepository.save(newMessage);
+            System.out.println("새로운 메세지가 작성되었습니다.");
+            return newMessage;
+        }
+        return null;
+    }
+
+    @Override
+    public List<Message> getAllMessageList() {
+        return messageRepository.findAll().values().stream().collect(Collectors.toList());
+    }
+
+    @Override
+    public Message searchById(UUID messageId) {
+        Message message = messageRepository.findById(messageId);
+        if(message == null) {
+            System.out.println("해당 메세지가 존재하지 않습니다.");
+        }
+        return message;
+    }
+
+    @Override
+    public void deleteMessage(UUID messageId) {
+        Message message = searchById(messageId);
+        if(message == null) {
+            System.out.println("해당 메세지가 존재하지 않습니다,");
+        } else {
+            messageRepository.delete(messageId);
+            System.out.println("해당 메세지가 삭제되었습니다.");
+        }
+
+    }
+
+    @Override
+    public void updateMessage(UUID messageId, String newContent) {
+        try {
+            Message message = searchById(messageId);
+            message.setContent(newContent);
+            messageRepository.save(message);
+            System.out.println(newContent+ "로 변경되었습니다.");
+        } catch (NoSuchElementException e) {
+            System.out.println("해당 메세지가 존재하지 않습니다.");
+            e.printStackTrace();
+        }
+    }
+
+    private boolean isContent(String content) {
+        if (content.isBlank()) {
+            System.out.println("내용을 입력해주세요!!!");
+            return false;
+        }
+        return true;
+    }
 }
