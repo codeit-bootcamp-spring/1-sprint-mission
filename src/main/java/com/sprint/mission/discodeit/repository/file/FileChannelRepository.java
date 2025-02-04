a8a0ddef47010f4f62a59ec72ce72f2bf03f819d
TaeHyoun Kim <kth9903@gmail.com>
2025-02-04 17:56:13 +0900

    chore(config): 파일 경로 주입 방식을 application.yaml로 변경 및 경로 수정

--- src/main/java/com/sprint/mission/discodeit/repository/file/FileChannelRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileChannelRepository.java
@@ -2,11 +2,13 @@ package com.sprint.mission.discodeit.repository.file;
 
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public class FileChannelRepository extends AbstractFileRepository<Channel> implements ChannelRepository {
 
-    public FileChannelRepository(String FILE_PATH) {
+    public FileChannelRepository(@Value("${data.path.channels}") String FILE_PATH) {
         super(FILE_PATH);
     }
 }
--- src/main/java/com/sprint/mission/discodeit/repository/file/FileUserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileUserRepository.java
@@ -2,13 +2,15 @@ package com.sprint.mission.discodeit.repository.file;
 
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public class FileUserRepository extends AbstractFileRepository<User> implements UserRepository {
 
-    public FileUserRepository(String FILE_PATH) {
+    public FileUserRepository(@Value("${data.path.users}") String FILE_PATH) {
         super(FILE_PATH);
     }
 
