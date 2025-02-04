d125f34cb594754352ba549ae4376e55b1ab5e36
TaeHyoun Kim <kth9903@gmail.com>
2025-02-04 17:40:45 +0900

    refactor: 에러 발생 시 로그 출력 로직 개선

--- src/main/java/com/sprint/mission/discodeit/repository/file/AbstractFileRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/AbstractFileRepository.java
@@ -1,11 +1,13 @@
 package com.sprint.mission.discodeit.repository.file;
 
 import com.sprint.mission.discodeit.entity.BaseEntity;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.*;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Slf4j
 public class AbstractFileRepository<T extends BaseEntity> {
     protected final Map<UUID, T> data;
     private final String FILE_PATH;
@@ -38,6 +40,7 @@ public class AbstractFileRepository<T extends BaseEntity> {
         return deleted;
     }
 
+    @SuppressWarnings("unchecked")
     private Map<UUID, T> loadData() {
         File file = new File(FILE_PATH);
         if (!file.exists()) {
@@ -55,7 +58,7 @@ public class AbstractFileRepository<T extends BaseEntity> {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_PATH))) {
             oos.writeObject(data);
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("저장에 실패했습니다. 요청 경로: {}", FILE_PATH, e);
         }
     }
 
