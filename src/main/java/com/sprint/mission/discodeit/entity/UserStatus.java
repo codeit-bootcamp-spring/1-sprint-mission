5f490bf3bdc25c800db0735a38d40fd7fcbda5f2
Kiki1875b <kevinheo0413@gmail.com>
2025-02-04 10:47:30 +0900

    feat : add UserStatus entity

---
+++ src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
@@ -0,0 +1,45 @@
+package com.sprint.mission.discodeit.entity;
+
+import com.sprint.mission.discodeit.util.UserStatusType;
+import com.sprint.mission.discodeit.util.UuidGenerator;
+import lombok.Getter;
+
+import java.time.Duration;
+import java.time.Instant;
+
+@Getter
+public class UserStatus {
+  private final String UUID;
+  private final String userId;
+  private final Instant createdAt;
+  private Instant lastOnlineAt;
+  private UserStatusType userStatus;
+  private Instant updatedAt;
+
+  public UserStatus(String userId, Instant lastOnlineAt) {
+    this.UUID = UuidGenerator.generateUUID();
+    this.userId = userId;
+    this.createdAt = Instant.now();
+    this.updatedAt = Instant.now();
+    this.lastOnlineAt = lastOnlineAt;
+    updateStatus();
+  }
+
+  public void updateLastOnline() {
+    Instant now = Instant.now();
+    updatedAt = now;
+    lastOnlineAt = now;
+    updateStatus();
+  }
+
+  private void updateStatus() {
+    long minutesSinceLastSeen = Duration.between(lastOnlineAt, Instant.now()).toMinutes();
+    if (minutesSinceLastSeen <= 5) {
+      userStatus = UserStatusType.ONLINE;
+    } else if (minutesSinceLastSeen <= 30) {
+      userStatus = UserStatusType.IDLE;
+    } else {
+      userStatus = UserStatusType.OFFLINE;
+    }
+  }
+}
