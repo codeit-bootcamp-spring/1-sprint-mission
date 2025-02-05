baedd88e5029568d71cee546814defceeadf4ec6
yudility <yudility@gmail.com>
2025-02-05 15:29:25 +0900

    feat: JCF기반 UserStatusRepository, ReadStatusRepository 구현

--- src/main/java/com/sprint/misson/discordeit/entity/status/ReadStatus.java
+++ src/main/java/com/sprint/misson/discordeit/entity/status/ReadStatus.java
@@ -1,7 +1,10 @@
 package com.sprint.misson.discordeit.entity.status;
 
+import lombok.Getter;
+
 import java.time.Instant;
 
+@Getter
 public class ReadStatus {
     private String id;
     private Instant createdAt;
