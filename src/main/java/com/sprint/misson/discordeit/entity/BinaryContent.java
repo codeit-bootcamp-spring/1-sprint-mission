35223717f27791211569b86c068d479b0e8876bb
yudility <yudility@gmail.com>
2025-02-05 15:41:29 +0900

    fix: BinaryContent @Getter 어노테이션 누락 수정

--- src/main/java/com/sprint/misson/discordeit/entity/BinaryContent.java
+++ src/main/java/com/sprint/misson/discordeit/entity/BinaryContent.java
@@ -1,7 +1,10 @@
 package com.sprint.misson.discordeit.entity;
 
+import lombok.Getter;
+
 import java.time.Instant;
 
+@Getter
 public class BinaryContent {
     private String id;
     private Instant createdAt;
