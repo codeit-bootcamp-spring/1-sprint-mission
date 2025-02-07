7fadc100ee0e6a0cfe1913762f7307a305ce0443
hyojkim2 <skql2548@gmail.com>
2025-02-07 13:44:19 +0900

    spring프로젝트 초기화

---
+++ src/test/java/com/spring/mission/discodeit/DiscodeitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mission.discodeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscodeitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
