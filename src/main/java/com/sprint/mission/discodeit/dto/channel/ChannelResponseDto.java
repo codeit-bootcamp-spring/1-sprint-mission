0b046483ea1b33fabcafcd8c7bd6a20fca28c87c
yudility <yudility@gmail.com>
2025-02-13 20:27:11 +0900

    feat: ChannelResponseDto 필드 추가

--- src/main/java/com/sprint/mission/discodeit/dto/channel/ChannelResponseDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/channel/ChannelResponseDto.java
@@ -1,22 +1,30 @@
 package com.sprint.mission.discodeit.dto.channel;
 
-import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.ChannelCategory;
 import com.sprint.mission.discodeit.entity.ChannelType;
+
+import java.time.Instant;
+import java.util.List;
 
 public record ChannelResponseDto(
         //채널 id
         String id,
         //채널명
         String channelName,
+        //채널 종류 - 공개, 비공개
+        ChannelType channelType,
         //채널 종류 - 음성, 텍스트
         ChannelCategory channelCategory,
-        //채널 공개 여부
-        ChannelType channelType,
-
-        String description
+        //채널 설명
+        String description,
+        //최근 메세지의 시간 정보
+        Instant lastMessageTimestamp,
+        //참여한 유저 정보
+        List<String> userIds
 ) {
-    public static ChannelResponseDto from(Channel channel) {
-        return new ChannelResponseDto(channel.getId(), channel.getChannelName(), channel.getChannelCategory(), channel.getChannelType(), channel.getDescription());
+    @Override
+    public String toString() {
+        return "[ChannelResponseDto] id=" + id + "/ channelName=" + channelName + "/ channelType=" + channelType + "/ channelCategory=" + channelCategory + "description="+description + "/ lastMessageTimestamp=" + lastMessageTimestamp + "/ userIds=" + userIds.stream().toList() + "]" ;
     }
+
 }
