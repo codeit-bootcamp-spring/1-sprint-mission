c4de8e100131bb42150dd755a074dc4a6ad3d96b
yudility <yudility@gmail.com>
2025-02-13 22:16:59 +0900

    feat: Service 계층 의존성 설정
    - BasicUserStatusService, BasicReadStatusService 검증 의존성 추가
    - BasicUserService 이미지 처리 의존성 추가
    - MessageService 빈 등록
    - Channel 구현체 BasicChannelService로 변경
    - [todo] 순환 참조 해결 필요 (Message <-> Channel)

--- src/main/java/com/sprint/mission/discodeit/config/AppConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/AppConfig.java
@@ -3,10 +3,7 @@ package com.sprint.mission.discodeit.config;
 import com.sprint.mission.discodeit.repository.*;
 import com.sprint.mission.discodeit.repository.jcf.*;
 import com.sprint.mission.discodeit.service.*;
-import com.sprint.mission.discodeit.service.basic.BasicBinaryContentService;
-import com.sprint.mission.discodeit.service.basic.BasicReadStatusService;
-import com.sprint.mission.discodeit.service.basic.BasicUserService;
-import com.sprint.mission.discodeit.service.basic.BasicUserStatusService;
+import com.sprint.mission.discodeit.service.basic.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,7 +11,9 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     @Bean
-    public UserRepository userRepository() {return new JCFUserRepository();}
+    public UserRepository userRepository() {
+        return new JCFUserRepository();
+    }
 
     @Bean
     public ChannelRepository channelRepository() {
@@ -43,12 +42,12 @@ public class AppConfig {
 
     @Bean
     public UserStatusService userStatusService() {
-        return new BasicUserStatusService(userStatusRepository());
+        return new BasicUserStatusService(userStatusRepository(), userRepository());
     }
 
     @Bean
     public ReadStatusService readStatusService() {
-        return new BasicReadStatusService(readStatusRepository());
+        return new BasicReadStatusService(readStatusRepository(), channelRepository(), userRepository());
     }
 
     @Bean
@@ -58,12 +57,17 @@ public class AppConfig {
 
     @Bean
     public UserService userService() {
-        return new BasicUserService(userRepository());
+        return new BasicUserService(userRepository(), binaryContentService());
+    }
+    //todo 순환참조 해결하기 - message <-> channel
+    @Bean
+    public MessageService messageService(){
+        return new BasicMessageService(messageRepository(), userService(), channelService(), binaryContentService())
     }
 
     @Bean
     public ChannelService channelService() {
-        return new FileChannelService(userService());
+        return new BasicChannelService(channelRepository(), userRepository(), messageService(), );
     }
 
 }
