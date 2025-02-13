1fa5aa1031b76f77fdafddea2cb0fd0b278cb505
yudility <yudility@gmail.com>
2025-02-13 22:12:39 +0900

    feat: ReadStatus 기능 구현
    - ReadStatus 생성 및 수정 기능 구현
    - 채널, 유저별 조회 기능 추가
    - 채널, 유저 의존성 추가

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicReadStatusService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicReadStatusService.java
@@ -1,7 +1,13 @@
 package com.sprint.mission.discodeit.service.basic;
 
+import com.sprint.mission.discodeit.code.ErrorCode;
+import com.sprint.mission.discodeit.dto.readStatus.CreateReadStatusDto;
+import com.sprint.mission.discodeit.dto.readStatus.UpdateReadStatusDto;
 import com.sprint.mission.discodeit.entity.status.ReadStatus;
+import com.sprint.mission.discodeit.exception.CustomException;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
 import com.sprint.mission.discodeit.repository.ReadStatusRepository;
+import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.service.ReadStatusService;
 import lombok.RequiredArgsConstructor;
 
@@ -11,24 +17,55 @@ import java.util.List;
 public class BasicReadStatusService implements ReadStatusService {
 
     private final ReadStatusRepository readStatusRepository;
+    private final ChannelRepository channelRepository;
+    private final UserRepository userRepository;
+
+    @Override
+    public ReadStatus create(CreateReadStatusDto createReadStatusDto) throws CustomException {
+        if (channelRepository.findById(createReadStatusDto.channelId()) == null) {
+            throw new CustomException(ErrorCode.CHANNEL_NOT_FOUND);
+        } else if (userRepository.findById(createReadStatusDto.userId()) == null) {
+            throw new CustomException(ErrorCode.USER_NOT_FOUND);
+        } else if (readStatusRepository.findByChannelIdWithUserId(createReadStatusDto.channelId(), createReadStatusDto.userId()) != null) {
+            throw new CustomException(ErrorCode.READ_STATUS_ALREADY_EXIST);
+        }
+
+        ReadStatus readStatus = new ReadStatus(createReadStatusDto.channelId(), createReadStatusDto.userId());
+        readStatusRepository.save(readStatus);
+
+        return readStatus;
+    }
 
     @Override
     public ReadStatus findById(String userStatusId) {
-        return null;
+        return readStatusRepository.findById(userStatusId);
     }
 
     @Override
-    public List<ReadStatus> findAll() {
-        return List.of();
+    public ReadStatus update(String id, UpdateReadStatusDto updateReadStatusDto) {
+        ReadStatus readStatus = readStatusRepository.findById(id);
+
+        if (readStatus == null) {
+            throw new CustomException(ErrorCode.READ_STATUS_NOT_FOUND);
+        }
+
+        if (readStatus.isUpdated(updateReadStatusDto)) {
+            return readStatusRepository.save(readStatus);
+        }
+        return readStatus;
+    }
+
+    public List<ReadStatus> findAllByUserId(String userId) {
+        return readStatusRepository.findAllByUserId(userId);
     }
 
     @Override
-    public boolean update(ReadStatus userStatus) {
-        return false;
+    public List<ReadStatus> findAllByChannelId(String channelId) {
+        return readStatusRepository.findAllByChannelId(channelId);
     }
 
     @Override
     public boolean delete(String userStatusId) {
-        return false;
+        return readStatusRepository.delete(userStatusId);
     }
 }
