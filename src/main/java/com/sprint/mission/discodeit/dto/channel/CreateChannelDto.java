6cfb09d8880390ac27aa36bb9a5a369386d10275
yudility <yudility@gmail.com>
2025-02-13 22:20:15 +0900

    chore: CreateChannelDto 미사용 import 제거
    - List import 제거

--- src/main/java/com/sprint/mission/discodeit/dto/channel/CreateChannelDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/channel/CreateChannelDto.java
@@ -3,11 +3,14 @@ package com.sprint.mission.discodeit.dto.channel;
 import com.sprint.mission.discodeit.entity.ChannelCategory;
 import com.sprint.mission.discodeit.entity.ChannelType;
 
+import java.util.List;
+
 public record CreateChannelDto(
         //채널명
         String channelName,
         ChannelType channelType,
         ChannelCategory channelCategory,
         String description
+
 ) {
 }
