9f02313d658ae30af81adfbe1167ad56e917f0c5
yudility <yudility@gmail.com>
2025-02-13 22:18:01 +0900

    refactor: UpdateMessageDto 불필요한 필드 제거
    - messageId 필드 제거 (메서드 파라미터로 전달)

--- src/main/java/com/sprint/mission/discodeit/dto/message/UpdateMessageDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/message/UpdateMessageDto.java
@@ -3,7 +3,6 @@ package com.sprint.mission.discodeit.dto.message;
 import java.time.Instant;
 
 public record UpdateMessageDto(
-        String messageId,
         String newContent,
         Instant updatedAt
 ){
