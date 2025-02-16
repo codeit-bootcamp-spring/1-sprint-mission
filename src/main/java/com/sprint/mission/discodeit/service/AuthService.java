853bcb7259f688957dda976b87a49975db0eebc7
yudility <yudility@gmail.com>
2025-02-16 19:10:48 +0900

    refactor: AuthService 로그인 응답 타입을 User에서 UserResponseDto로 변경
    - Entity 직접 노출을 방지하기 위해 응답 타입을 DTO로 변경
    - User 엔티티 의존성 제거

--- src/main/java/com/sprint/mission/discodeit/service/AuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/AuthService.java
@@ -1,10 +1,10 @@
 package com.sprint.mission.discodeit.service;
 
 import com.sprint.mission.discodeit.dto.user.UserLoginDto;
-import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.dto.user.UserResponseDto;
 
 public interface AuthService {
 
-    User login(UserLoginDto loginDto);
+    UserResponseDto login(UserLoginDto loginDto);
 
 }
