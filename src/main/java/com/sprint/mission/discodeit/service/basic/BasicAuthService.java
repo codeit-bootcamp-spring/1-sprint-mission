87302f1d63caee981803ae6baae3b3ce223b876a
yudility <yudility@gmail.com>
2025-02-16 19:12:15 +0900

    feat: 로그인 시 사용자 상태 업데이트 및 응답 DTO 변환 기능 추가
    - UserStatusService 의존성 추가하여 로그인 시 접속 시간 갱신
    - 사용자 온라인 상태 정보 포함하도록 수정
    - User 엔티티를 UserResponseDto로 변환하여 반환하도록 변경

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -2,23 +2,33 @@ package com.sprint.mission.discodeit.service.basic;
 
 import com.sprint.mission.discodeit.code.ErrorCode;
 import com.sprint.mission.discodeit.dto.user.UserLoginDto;
+import com.sprint.mission.discodeit.dto.user.UserResponseDto;
+import com.sprint.mission.discodeit.dto.userStatus.UpdateUserStatusDto;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.exception.CustomException;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.service.AuthService;
+import com.sprint.mission.discodeit.service.UserStatusService;
 import lombok.RequiredArgsConstructor;
+
+import java.time.Instant;
 
 @RequiredArgsConstructor
 public class BasicAuthService implements AuthService {
 
     private final UserRepository userRepository;
+    private final UserStatusService userStatusService;
 
     @Override
-    public User login(UserLoginDto userLoginDto) throws CustomException {
+    public UserResponseDto login(UserLoginDto userLoginDto) throws CustomException {
         User user = userRepository.findById(userLoginDto.username());
         if(user == null || !user.getPassword().equals(userLoginDto.password()) ) {
             throw new CustomException(ErrorCode.USER_NOT_FOUND);
         }
-        return user;
+        // 이것도 마찬가지로 어할 수 없는 값이라 이 방식을 쓰면 안되는지?
+        // User 접속 시간 갱신 및 온라인 정보 가져오기
+        boolean isUserOnline = userStatusService.updateByUserId(user.getId(), new UpdateUserStatusDto(user.getId(), Instant.now())).isActive();
+
+        return UserResponseDto.from(user, isUserOnline);
     }
 }
