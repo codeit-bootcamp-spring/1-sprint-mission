dc11dbea05eebe3799f6d8b36bd5b7b1d5c3d174
yudility <yudility@gmail.com>
2025-02-16 18:54:07 +0900

    feat: Repository 의존성 주입 방식 개선 및 서비스 계층 의존관계 수정
    - Repository 클래스들의 직접 생성 방식을 생성자 주입 방식으로 변경
    - UserService에 UserStatusService 의존성 추가
    - ChannelService에 UserStatusService 의존성 추가
    - @RequiredArgsConstructor 어노테이션 추가로 생성자 주입 자동화

--- src/main/java/com/sprint/mission/discodeit/config/AppConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/AppConfig.java
@@ -1,73 +1,51 @@
 package com.sprint.mission.discodeit.config;
 
 import com.sprint.mission.discodeit.repository.*;
-import com.sprint.mission.discodeit.repository.jcf.*;
 import com.sprint.mission.discodeit.service.*;
 import com.sprint.mission.discodeit.service.basic.*;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class AppConfig {
 
-    @Bean
-    public UserRepository userRepository() {
-        return new JCFUserRepository();
-    }
-
-    @Bean
-    public ChannelRepository channelRepository() {
-        return new JCFChannelRepository();
-    }
-
-    @Bean
-    public MessageRepository messageRepository() {
-        return new JCFMessageRepository();
-    }
-
-    @Bean
-    public UserStatusRepository userStatusRepository() {
-        return new JCFUserStatusRepository();
-    }
-
-    @Bean
-    public ReadStatusRepository readStatusRepository() {
-        return new JCFReadStatusRepository();
-    }
-
-    @Bean
-    public BinaryContentRepository binaryContentRepository() {
-        return new JCFBinaryContentRepository();
-    }
+    private final ChannelRepository channelRepository;
+    private final MessageRepository messageRepository;
+    private final UserRepository userRepository;
+    private final UserStatusRepository userStatusRepository;
+    private final BinaryContentRepository binaryContentRepository;
+    private final ReadStatusRepository readStatusRepository;
 
     @Bean
     public UserStatusService userStatusService() {
-        return new BasicUserStatusService(userStatusRepository(), userRepository());
+        return new BasicUserStatusService(userStatusRepository, userRepository);
     }
 
     @Bean
     public ReadStatusService readStatusService() {
-        return new BasicReadStatusService(readStatusRepository(), channelRepository(), userRepository());
+        return new BasicReadStatusService(readStatusRepository, channelRepository, userRepository);
     }
 
     @Bean
     public BinaryContentService binaryContentService() {
-        return new BasicBinaryContentService(binaryContentRepository());
+        return new BasicBinaryContentService(binaryContentRepository);
     }
 
     @Bean
     public UserService userService() {
-        return new BasicUserService(userRepository(), binaryContentService());
+        return new BasicUserService(userRepository, userStatusService() ,binaryContentService());
     }
     //todo 순환참조 해결하기 - message <-> channel
     @Bean
     public MessageService messageService(){
-        return new BasicMessageService(messageRepository(), userService(), channelService(), binaryContentService());
+        return new BasicMessageService(messageRepository, userService(), channelService(), binaryContentService());
     }
 
     @Bean
     public ChannelService channelService() {
-        return new BasicChannelService(channelRepository(), userRepository(), messageRepository(), readStatusService());
+        return new BasicChannelService(channelRepository, userRepository, messageRepository, readStatusService(), userStatusService());
     }
 
 }
