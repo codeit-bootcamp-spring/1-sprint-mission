e2b30e712f100a0d7a3b31e9365a258ac68f396e
yudility <yudility@gmail.com>
2025-02-20 21:33:46 +0900

    fix: @SpringBootApplication 적용
    - DiscodeitApplication의 기존 테스트 코드 주석 처리
    - @SpringBootApplication 적용

--- src/main/java/com/sprint/mission/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/DiscodeitApplication.java
@@ -1,3 +1,20 @@
+package com.sprint.mission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class DiscodeitApplication {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(DiscodeitApplication.class, args);
+        Environment env = context.getEnvironment();
+        System.out.println("Repository Type: " + env.getProperty("discodeit.repository.type"));
+        System.out.println("File Directory: " + env.getProperty("discodeit.repository.file-directory"));
+    }
+}
+
 /*
 package com.sprint.mission;
 
