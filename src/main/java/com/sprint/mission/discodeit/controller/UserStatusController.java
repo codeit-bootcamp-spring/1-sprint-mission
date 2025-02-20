4e0cbcc646a02d3825cdc0828e23bffeb343176d
yudility <yudility@gmail.com>
2025-02-20 22:04:39 +0900

    fix: userstatus api uri 수정

--- src/main/java/com/sprint/mission/discodeit/controller/UserStatusController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserStatusController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users/status")
+@RequestMapping("/user-status")
 @RequiredArgsConstructor
 public class UserStatusController {
 
