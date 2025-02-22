3803a90cff21ebcbcef72c457f5110cacee909d2
Kiki1875b <kevinheo0413@gmail.com>
2025-02-22 16:34:09 +0900

    feat : added Swagger Doc for AuthController

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.controller.openapi.AuthApiDocs;
 import com.sprint.mission.discodeit.dto.auth.LoginDto;
 import com.sprint.mission.discodeit.dto.user.LoginResponseDto;
 import com.sprint.mission.discodeit.service.AuthService;
@@ -15,9 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
-public class AuthController {
+public class AuthController implements AuthApiDocs {
 
   private final AuthService authService;
+  @Override
   @PostMapping("/login")
   public ResponseEntity<LoginResponseDto> userLogin(@RequestBody LoginDto loginDto){
     LoginResponseDto user = authService.login(loginDto.username(), loginDto.password());
