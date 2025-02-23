56d456cf511e1b0bd686eb36b0617843dff0a796
kdi <66400384+kbkg11@users.noreply.github.com>
2025-02-23 14:59:30 +0900

    미션4 베이스 코드

--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -1,18 +1,19 @@
 package com.sprint.mission.discodeit.service;
 
+import com.sprint.mission.discodeit.dto.data.UserDto;
+import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
+import com.sprint.mission.discodeit.dto.request.UserCreateRequest;
+import com.sprint.mission.discodeit.dto.request.UserUpdateRequest;
 import com.sprint.mission.discodeit.entity.User;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-/*
- *  CRUD(생성, 읽기, 모두 읽기, 수정, 삭제) 기능을 선언한 인터페이스
- * */
-
 public interface UserService {
-    void addUser(User user);
-    User getUser(UUID id);
-    List<User> getAllUsers();
-    void updateUser(UUID id, String newUsername, String newPassword);
-    void deleteUser(UUID id);
+    User create(UserCreateRequest userCreateRequest, Optional<BinaryContentCreateRequest> profileCreateRequest);
+    UserDto find(UUID userId);
+    List<UserDto> findAll();
+    User update(UUID userId, UserUpdateRequest userUpdateRequest, Optional<BinaryContentCreateRequest> profileCreateRequest);
+    void delete(UUID userId);
 }
