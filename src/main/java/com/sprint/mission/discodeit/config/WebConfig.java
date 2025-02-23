d17058f8359ebf5ffb97ee5239dce3d6cc854140
Kiki1875b <kevinheo0413@gmail.com>
2025-02-23 17:24:55 +0900

    feat : added interceptor to log incoming request and outgoing response

---
+++ src/main/java/com/sprint/mission/discodeit/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.sprint.mission.discodeit.config;
+
+import com.sprint.mission.discodeit.interceptor.RequestLoggingInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new RequestLoggingInterceptor())
+        .order(1)
+        .addPathPatterns("/**");
+  }
+}
---
+++ src/main/java/com/sprint/mission/discodeit/interceptor/RequestLoggingInterceptor.java
@@ -0,0 +1,28 @@
+package com.sprint.mission.discodeit.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+@Component
+public class RequestLoggingInterceptor implements HandlerInterceptor {
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    log.info("[Incoming Request] : [{}][{}]", request.getMethod(), request.getRequestURI());
+    return true;
+  }
+
+  @Override
+  public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    log.info("[Request Complete] : [{}][{}] | [Status Code] : [{}]", request.getMethod(), request.getRequestURI(), response.getStatus());
+
+    if(ex != null){
+      log.info("[Error] : [{}] : [{}]", ex, ex.getMessage());
+    }
+  }
+}
