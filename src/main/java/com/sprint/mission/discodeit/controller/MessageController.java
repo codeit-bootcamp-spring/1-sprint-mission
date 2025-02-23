ec9b22a5863f1c30d53f28037a67d349f96df695
yudility <yudility@gmail.com>
2025-02-23 17:29:33 +0900

    feat: 특정 채널의 모든 메세지 목록 조회 api 구현 및 검증 추가
    - 전체 메세지 조회 기능 추가
    - 메세지 수정, 삭제 시 요청하는 사용자가 작성자인지 검증하도록 수정
    - 메세지 update dto 검증 로직 리팩토링
    - 메세지 서비스 구현체 create() 리팩토링

--- src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -17,28 +17,39 @@ public class MessageController {
 
     private final MessageService messageService;
 
-    //todo - 특정 채널 메세지 생성
+    @GetMapping("/all")
+    public List<MessageResponseDto> getAllMessages() {
+        return messageService.findAll();
+    }
+
+    //특정 채널 메세지 생성
     @PostMapping
-    public MessageResponseDto getMessages(@RequestBody CreateMessageDto createMessageDto) {
+    public MessageResponseDto createMessage(@RequestBody CreateMessageDto createMessageDto) {
         return messageService.create(createMessageDto);
     }
 
-    //todo - 특정 채널 메세지 수정
+    //특정 채널 메세지 수정
     @PutMapping("/{messageId}")
     public MessageResponseDto updateMessage(@PathVariable String messageId, @RequestBody UpdateMessageDto updateMessageDto) {
         return messageService.updateMessage(messageId, updateMessageDto);
     }
 
-    //todo - 특정 사용자의 모든 메세지 목록 조회
+    //특정 사용자의 모든 메세지 목록 조회
     @GetMapping("/users/{userId}")
-    public List<MessageResponseDto> getMessages(@PathVariable String userId) {
+    public List<MessageResponseDto> getMessagesByUserId(@PathVariable String userId) {
         return messageService.findAllBySenderId(userId);
+    }
+
+    //특정 채널의 모든 메세지 목록 조회
+    @GetMapping("/channels/{channelId}")
+    public List<MessageResponseDto> getMessagesByChannelId(@PathVariable String channelId) {
+        return messageService.findAllByChannelId(channelId);
     }
 
     //메세지 삭제
     @DeleteMapping("/{messageId}")
-    public String  deleteMessage(@PathVariable String messageId) {
-        if (messageService.delete(messageId)) {
+    public String deleteMessage(@PathVariable String messageId, @RequestParam String userId) {
+        if (messageService.delete(messageId, userId)) {
             return "Message deleted successfully";
         }
         return "Message not found";
