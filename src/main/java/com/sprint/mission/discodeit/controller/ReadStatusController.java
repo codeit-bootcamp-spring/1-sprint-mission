c5b30826cba7dd72ffebc469f2e6ce489fec24bd
Oince <39307905+Oince@users.noreply.github.com>
2025-02-23 21:47:01 +0900

    ReadStatusController 개발

---
+++ src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
@@ -0,0 +1,38 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.dto.request.ReadStatusRequest;
+import com.sprint.mission.discodeit.dto.response.ReadStatusResponse;
+import com.sprint.mission.discodeit.entity.ReadStatus;
+import com.sprint.mission.discodeit.service.basic.ReadStatusService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/read_statuses")
+public class ReadStatusController {
+
+    private final ReadStatusService readStatusService;
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody ReadStatusRequest readStatusRequest) {
+        ReadStatus readStatus = readStatusService.create(readStatusRequest);
+        return ResponseEntity.created(URI.create("/read_statuses/" + readStatus.getId())).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ReadStatusResponse> find(@PathVariable UUID id) {
+        ReadStatus readStatus = readStatusService.find(id);
+        return ResponseEntity.ok(ReadStatusResponse.from(readStatus));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable UUID id) {
+        readStatusService.update(id);
+        return ResponseEntity.ok().build();
+    }
+}
