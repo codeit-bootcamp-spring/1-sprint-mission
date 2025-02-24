44c99643c091a88bae0e57f2ab34daf75ecee98d
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-02-24 16:19:26 +0900

    프로젝트 베이스 5 코드 가져오기

---
+++ src/main/java/com/sprint/mission/discodeit/config/SwaggerConfiguration.java
@@ -0,0 +1,25 @@
+package com.sprint.mission.discodeit.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+  @Bean
+  public OpenAPI openAPI() {
+    return new OpenAPI()
+        .components(new Components())
+        .info(apiInfo());
+  }
+
+  private Info apiInfo() {
+    return new Info()
+        .title("Code-it Spring Mission REST-API")
+        .description("스프린트 미션 REST API")
+        .version("1.0.0");
+  }
+}
