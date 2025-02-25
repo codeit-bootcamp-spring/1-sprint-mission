0d30fb2b04032c2c17b56c70f43d2f1a3bba6bad
narone <buzzin2426@gmail.com>
2025-02-25 16:13:33 +0900

    sprint mission 5

--- src/main/java/com/sprint/mission/discodeit/controller/api/AuthApi.java
+++ src/main/java/com/sprint/mission/discodeit/controller/api/AuthApi.java
@@ -18,8 +18,20 @@ public interface AuthApi {
 
   @Operation(summary = "로그인")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "로그인 성공", content = @Content(schema = @Schema(implementation = UserDto.class))),
-      @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음", content = @Content(examples = @ExampleObject(value = "User not found"))),
-      @ApiResponse(responseCode = "400", description = "비밀번호가 일치하지 않음", content = @Content(examples = @ExampleObject(value = "Invalid password")))})
-  ResponseEntity<UserDto> login(@Parameter(description = "로그인 정보") LoginRequest loginRequest);
+      @ApiResponse(
+          responseCode = "200", description = "로그인 성공",
+          content = @Content(schema = @Schema(implementation = UserDto.class))
+      ),
+      @ApiResponse(
+          responseCode = "404", description = "사용자를 찾을 수 없음",
+          content = @Content(examples = @ExampleObject(value = "User not found"))
+      ),
+      @ApiResponse(
+          responseCode = "400", description = "비밀번호가 일치하지 않음",
+          content = @Content(examples = @ExampleObject(value = "Invalid password"))
+      )
+  })
+  ResponseEntity<UserDto> login(
+      @Parameter(description = "로그인 정보") LoginRequest loginRequest
+  );
 }
--- src/main/java/com/sprint/mission/discodeit/controller/api/UserApi.java
+++ src/main/java/com/sprint/mission/discodeit/controller/api/UserApi.java
@@ -26,35 +26,74 @@ public interface UserApi {
 
   @Operation(summary = "User 등록")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "201", description = "User가 성공적으로 생성됨", content = @Content(schema = @Schema(implementation = UserDto.class))),
-      @ApiResponse(responseCode = "400", description = "같은 email 또는 username를 사용하는 User가 이미 존재함", content = @Content(examples = @ExampleObject(value = "email | username already exists: {email | username}"))),})
+      @ApiResponse
+          (responseCode = "201", description = "User가 성공적으로 생성됨",
+              content = @Content(schema = @Schema(implementation = UserDto.class))
+          ),
+      @ApiResponse(
+          responseCode = "400", description = "같은 email 또는 username를 사용하는 User가 이미 존재함",
+          content = @Content(examples = @ExampleObject(value = "email | username already exists: {email | username}"))
+      )
+  })
   ResponseEntity<UserDto> createUser(
-      @Parameter(description = "User 생성 정보", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)) UserCreateRequest userCreateRequest,
-      @Parameter(description = "User 프로필 이미지", content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE)) MultipartFile profile);
+      @Parameter(
+          description = "User 생성 정보",
+          content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)) UserCreateRequest userCreateRequest,
+      @Parameter(
+          description = "User 프로필 이미지",
+          content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE)) MultipartFile profile);
 
   @Operation(summary = "User 정보 수정")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "User 정보가 성공적으로 수정됨", content = @Content(schema = @Schema(implementation = UserDto.class))),
-      @ApiResponse(responseCode = "404", description = "User를 찾을 수 없음", content = @Content(examples = @ExampleObject("User not found: {userId}"))),
-      @ApiResponse(responseCode = "400", description = "같은 email 또는 username를 사용하는 User가 이미 존재함", content = @Content(examples = @ExampleObject("Email already exists: {newEmail}")))})
+      @ApiResponse(
+          responseCode = "200",
+          description = "User 정보가 성공적으로 수정됨", content = @Content(schema = @Schema(implementation = UserDto.class))
+      ),
+      @ApiResponse(
+          responseCode = "404",
+          description = "User를 찾을 수 없음", content = @Content(examples = @ExampleObject("User not found: {userId}"))
+      ),
+      @ApiResponse(
+          responseCode = "400", description = "같은 email 또는 username를 사용하는 User가 이미 존재함",
+          content = @Content(examples = @ExampleObject("Email already exists: {newEmail}"))
+      )
+  })
   ResponseEntity<UserDto> updateUser(@Parameter(description = "수정할 User ID") UUID userId,
       @Parameter(description = "수정할 User 정보") UserUpdateRequest userUpdateRequest,
       @Parameter(description = "수정할 User 프로필 이미지") MultipartFile profile);
 
   @Operation(summary = "User 삭제")
-  @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "User가 성공적으로 삭제됨"),
-      @ApiResponse(responseCode = "404", description = "User를 찾을 수 없음", content = @Content(examples = @ExampleObject(value = "User not found: {userId}")))})
+  @ApiResponses(value = {
+      @ApiResponse(
+          responseCode = "204", description = "User가 성공적으로 삭제됨"
+      ),
+      @ApiResponse(
+          responseCode = "404", description = "User를 찾을 수 없음",
+          content = @Content(examples = @ExampleObject(value = "User not found: {userId}"))
+      )
+  })
   ResponseEntity<Void> deleteUser(@Parameter(description = "삭제할 User ID") UUID userId);
 
   @Operation(summary = "전체 User 목록 조회")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "User 목록 조회 성공", content = @Content(array = @ArraySchema(schema = @Schema(implementation = UserDto.class))))})
+      @ApiResponse(
+          responseCode = "200", description = "User 목록 조회 성공",
+          content = @Content(array = @ArraySchema(schema = @Schema(implementation = UserDto.class)))
+      )
+  })
   ResponseEntity<List<UserDto>> getAllUsers();
 
   @Operation(summary = "User 온라인 상태 업데이트")
   @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "User 온라인 상태가 성공적으로 업데이트됨", content = @Content(schema = @Schema(implementation = UserStatusDto.class))),
-      @ApiResponse(responseCode = "404", description = "해당 User의 UserStatus를 찾을 수 없음", content = @Content(examples = @ExampleObject(value = "UserStatus not found: userId = {userId}")))})
+      @ApiResponse(
+          responseCode = "200", description = "User 온라인 상태가 성공적으로 업데이트됨",
+          content = @Content(schema = @Schema(implementation = UserStatusDto.class))
+      ),
+      @ApiResponse(
+          responseCode = "404", description = "해당 User의 UserStatus를 찾을 수 없음",
+          content = @Content(examples = @ExampleObject(value = "UserStatus not found: userId = {userId}"))
+      )
+  })
   ResponseEntity<UserStatusDto> updateUserStatusByUserId(
       @Parameter(description = "상태를 변경할 User ID") UUID userId,
       @Parameter(description = "변경할 User 온라인 상태 정보") UserStatusUpdateRequest request);
