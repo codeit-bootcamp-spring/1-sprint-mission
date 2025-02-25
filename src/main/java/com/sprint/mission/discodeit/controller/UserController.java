2e867c19535647cb0aee5adc1513456b96c8f132
Yang-ByeongUn <ypd06426@naver.com>
2025-02-25 18:42:06 +0900

    Removed embedded Git repository

--- src/main/java/com/sprint/mission/discodeit/controller/UserController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -23,7 +23,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
+//@Controller
 @RequestMapping("/api/users")
 public class UserController implements UserApiDocs {
 
