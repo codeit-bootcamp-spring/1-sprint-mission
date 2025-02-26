f78c31883a07c51c66cf2dddd67eb5409683ebaa
yudility <yudility@gmail.com>
2025-02-26 12:06:16 +0900

    fix: userStatus API URI 수정
    - getUserStatus() 삭제

--- src/main/java/com/sprint/mission/discodeit/controller/UserStatusController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserStatusController.java
@@ -3,24 +3,19 @@ package com.sprint.mission.discodeit.controller;
 import com.sprint.mission.discodeit.dto.userStatus.UpdateUserStatusDto;
 import com.sprint.mission.discodeit.dto.userStatus.UserStatusResponseDto;
 import com.sprint.mission.discodeit.service.UserStatusService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/user-status")
+@RequestMapping("/api/userStatus")
 @RequiredArgsConstructor
 @Tag(name = "User Status", description = "사용자 온라인 상태 API")
 public class UserStatusController {
 
   private final UserStatusService userStatusService;
-
-  @PutMapping("/{userId}")
-  public UserStatusResponseDto getUserStatus(@PathVariable String userId,
-      @RequestBody UpdateUserStatusDto updateUserStatusDto) {
-    return userStatusService.updateByUserId(userId, updateUserStatusDto);
-  }
 
   @GetMapping
   public List<UserStatusResponseDto> getAllUserStatus() {
