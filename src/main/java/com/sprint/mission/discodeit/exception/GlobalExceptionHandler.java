67e7cffc18932e86133cd520f2de752f9cfb8067
yudility <yudility@gmail.com>
2025-02-26 12:03:57 +0900

    feat: FileUploadException 예외 처리 추가
    - API 요청 시 파일이 없는 경우 발생하는 에러 처리
    - int 형 에러코드를 HttpStatus로 변경하여 가독성 향상

--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -3,7 +3,9 @@ package com.sprint.mission.discodeit.exception;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
 import org.apache.tomcat.util.http.fileupload.impl.InvalidContentTypeException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,32 +15,44 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
-        ErrorResponse errorResponse = new ErrorResponse(
-                e.getErrorCode().getCode(),
-                e.getMessage(),
-                LocalDateTime.now()
-        );
-        return ResponseEntity.badRequest().body(errorResponse);
-    }
+  @ExceptionHandler(CustomException.class)
+  public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+    ErrorResponse errorResponse = new ErrorResponse(
+        e.getErrorCode().getCode(),
+        e.getMessage(),
+        LocalDateTime.now()
+    );
+    return ResponseEntity.badRequest().body(errorResponse);
+  }
 
-    @ExceptionHandler(InvalidContentTypeException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidContentTypeException(InvalidContentTypeException e) {
-        ErrorResponse errorResponse = new ErrorResponse(
-                400,
-                e.getMessage(),
-                LocalDateTime.now()
-        );
-        return ResponseEntity.badRequest().body(errorResponse);
-    }
+  @ExceptionHandler(FileUploadException.class)
+  public ResponseEntity<ErrorResponse> handleFileUploadException(FileUploadException e) {
+    ErrorResponse errorResponse = new ErrorResponse(
+        HttpStatus.BAD_REQUEST.value(),
+        "The request was rejected because no multipart boundary was found",
+        LocalDateTime.now()
+    );
+    return ResponseEntity.badRequest().body(errorResponse);
+  }
 
-    @Getter
-    @Setter
-    @AllArgsConstructor
-    public static class ErrorResponse{
-        private int errorCode;
-        private String errorMessage;
-        private LocalDateTime timestamp;
-    }
+  @ExceptionHandler(InvalidContentTypeException.class)
+  public ResponseEntity<ErrorResponse> handleInvalidContentTypeException(
+      InvalidContentTypeException e) {
+    ErrorResponse errorResponse = new ErrorResponse(
+        HttpStatus.BAD_REQUEST.value(),
+        e.getMessage(),
+        LocalDateTime.now()
+    );
+    return ResponseEntity.badRequest().body(errorResponse);
+  }
+
+  @Getter
+  @Setter
+  @AllArgsConstructor
+  public static class ErrorResponse {
+
+    private int errorCode;
+    private String errorMessage;
+    private LocalDateTime timestamp;
+  }
 }
