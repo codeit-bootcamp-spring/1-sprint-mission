c98f504f29f7ed6c3f5e3ed5c9f448f56dc5c3f3
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-03-05 16:28:37 +0900

    CREATE: schema file

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+-- USER
+CREATE TABLE users
+(
+    user_id UUID,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    CONSTRAINT
+);
+
+
+
+
