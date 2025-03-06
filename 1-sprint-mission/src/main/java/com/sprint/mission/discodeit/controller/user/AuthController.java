1896f97ae03ddbba766d1919c1f9443b87726106
kbh <xjvm7001@naver.com>
2025-03-06 13:31:25 +0900

    Auth API Refactoring

--- 1-sprint-mission/src/main/java/com/sprint/mission/discodeit/controller/user/AuthController.java
+++ 1-sprint-mission/src/main/java/com/sprint/mission/discodeit/controller/user/AuthController.java
@@ -6,6 +6,7 @@ import com.sprint.mission.discodeit.dto.response.user.UserLoginResponseDTO;
 import com.sprint.mission.discodeit.service.interfacepac.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/login")
+@RequestMapping("/api/auth")
 @Validated
 @Tag(name = "UserLogin Controller", description = "사용자 로그인 관련 API 앤드포인트 관리")
 public class UserLoginController {
@@ -26,10 +27,12 @@ public class UserLoginController {
   }
 
   @Operation(summary = "사용자 로그인", description = "사용자 로그인 처리 앤드 포인트")
-  @PostMapping
+  @PostMapping("/login")
   public ResponseEntity<UserLoginResponseDTO> login(
       @RequestBody UserLoginRequestDTO userLoginRequestDTO) {
     UserLoginResponseDTO login = authService.login(userLoginRequestDTO);
-    return ResponseEntity.ok(login);
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .body(login);
   }
 }
--- 1-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ 1-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -19,38 +19,36 @@ import java.util.NoSuchElementException;
 @RequiredArgsConstructor
 public class BasicAuthService implements AuthService {
 
-    private final UserRepository userRepository;
-    private final UserStatusRepository userStatusRepository;
+  private final UserRepository userRepository;
+  private final UserStatusRepository userStatusRepository;
 
 
-    @Override
-    public UserLoginResponseDTO login(UserLoginRequestDTO userLoginRequestDTO) {
-        //사용자 확인
-        User user = userRepository.findByEmail(userLoginRequestDTO.email())
-                .orElseThrow(() -> new NoSuchElementException("Invalid email"));
-        //비밀번호 확인
-        if(!user.getPassword().equals(userLoginRequestDTO.password())){
-            throw new IllegalArgumentException("Wrong password");
-        }
-        //사용자 상태 확인 및 업데이트
-        UserStatus userStatus = userStatusRepository.findByUserId(user.getId())
-                .orElse(new UserStatus(user, Instant.now()));
-        //로그인 시 마지막 활동 시간 업데이트
-        userStatus.updateLastSeenAt(Instant.now());
-        userStatusRepository.save(userStatus);
-
-        //온라인 상태 확인
-        boolean isOnline = userStatus.isOnline();
-
-
-        log.info("User Online : {} ", isOnline);
-        return new UserLoginResponseDTO(
-                user.getId(),
-                user.getUsername(),
-                user.getEmail(),
-                user.getCreatedAt(),
-                user.getUpdatedAt(),
-                isOnline
-        );
+  @Override
+  public UserLoginResponseDTO login(UserLoginRequestDTO userLoginRequestDTO) {
+    //사용자 확인
+    User user = userRepository.findByEmail(userLoginRequestDTO.email())
+        .orElseThrow(() -> new NoSuchElementException("Invalid email"));
+    //비밀번호 확인
+    if (!user.getPassword().equals(userLoginRequestDTO.password())) {
+      throw new IllegalArgumentException("Wrong password");
     }
+    //사용자 상태 확인 및 업데이트
+    UserStatus userStatus = userStatusRepository.findByUserId(user.getId())
+        .orElse(new UserStatus(user, Instant.now()));
+    //로그인 시 마지막 활동 시간 업데이트
+    userStatus.updateLastSeenAt(Instant.now());
+    userStatusRepository.save(userStatus);
+
+    //온라인 상태 확인
+    boolean isOnline = userStatus.isOnline();
+
+    return new UserLoginResponseDTO(
+        user.getId(),
+        user.getUsername(),
+        user.getEmail(),
+        user.getCreatedAt(),
+        user.getUpdatedAt(),
+        isOnline
+    );
+  }
 }
