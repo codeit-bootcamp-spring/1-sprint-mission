e0fa9a2c2d635451decba725de842e06f842d283
yudility <yudility@gmail.com>
2025-03-12 17:38:42 +0900

    feat: BaseEntity @MappedSuperclass 추가

--- src/main/java/com/sprint/mission/discodeit/entity/base/BaseEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/base/BaseEntity.java
@@ -1,14 +1,23 @@
 package com.sprint.mission.discodeit.entity.base;
 
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import java.time.Instant;
 import java.util.UUID;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 
 @Getter
+@MappedSuperclass
 public abstract class BaseEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   UUID id;
+
   @CreatedDate
   Instant createdAt;
 
--- src/main/java/com/sprint/mission/discodeit/entity/base/BaseUpdatableEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/base/BaseUpdatableEntity.java
@@ -1,5 +1,6 @@
 package com.sprint.mission.discodeit.entity.base;
 
+import jakarta.persistence.MappedSuperclass;
 import java.time.Instant;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import org.springframework.data.annotation.LastModifiedBy;
 
 @Getter
 @Setter
+@MappedSuperclass
 public abstract class BaseUpdatableEntity extends BaseEntity {
 
   @LastModifiedBy
