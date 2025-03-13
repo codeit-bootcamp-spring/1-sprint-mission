43f2b997b78db3fc0062aafb39e666124fb71fff
yudility <yudility@gmail.com>
2025-03-13 10:02:42 +0900

    fix: Channel, BinaryContent 삭제 수정

--- src/main/java/com/sprint/mission/discodeit/repository/ChannelRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ChannelRepository.java
@@ -11,9 +11,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface ChannelRepository extends JpaRepository<Channel, UUID> {
 
-  //delete
-  boolean delete(UUID id);
-
   //유저가 속한 채널만 가져오는 쿼리
   //Channels과 ReadStatuses를 fetch 조인 하여 user_id가 같은 channel들만 가져옴
   @Query("SELECT DISTINCT c FROM Channel c " +
@@ -28,7 +25,7 @@ public interface ChannelRepository extends JpaRepository<Channel, UUID> {
       "JOIN FETCH c.users rs " +
       "JOIN FETCH rs.user u " +
       "LEFT JOIN FETCH u.userStatus " +
-      "WHERE c.id = :channelId AND c.type = 'PRIVATE'")
+      "WHERE c.id = :channelId AND c.type = com.sprint.mission.discodeit.entity.ChannelType.PRIVATE")
   Optional<Channel> findPrivateChannelWithParticipantsById(@Param("channelId") UUID channelId);
-  
+
 }
