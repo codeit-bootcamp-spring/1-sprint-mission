c5da2f219c9b23bc64cdede3b913e0e19e1ed185
yudility <yudility@gmail.com>
2025-03-13 10:12:24 +0900

    fix: findByChannelId, findByAuthorId의 id 형식 불일치 수정

--- src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -2,12 +2,12 @@ package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.Message;
 
-import java.awt.print.Pageable;
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,9 +16,9 @@ import org.springframework.data.repository.query.Param;
 
 public interface MessageRepository extends JpaRepository<Message, UUID> {
 
-  List<Message> findByChannelId(String channelId);
+  List<Message> findByChannelId(UUID channelId);
 
-  Optional<Message> findByAuthorId(String authorId);
+  Optional<Message> findByAuthorId(UUID authorId);
 
   List<Message> findByContentContains(String content);
 
