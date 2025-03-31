59f1d33067e48159b97cc3b12818b37af1b25f07
kbh <xjvm7001@naver.com>
2025-03-31 18:16:26 +0900

    GlobalExceptionHandler 리팩토링

--- 1-sprint-mission/src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ 1-sprint-mission/src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.sprint.mission.discodeit.exception;
 
+import com.sprint.mission.discodeit.dto.response.ErrorResponse;
+import java.time.Instant;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,27 +13,33 @@ import java.util.NoSuchElementException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-  @ExceptionHandler(IllegalArgumentException.class)
-  public ResponseEntity<String> handleException(IllegalArgumentException e) {
-    e.printStackTrace();
-    return ResponseEntity
-        .status(HttpStatus.BAD_REQUEST)
-        .body(e.getMessage());
-  }
+  @ExceptionHandler(DiscodeitException.class)
+  public ResponseEntity<ErrorResponse> handleDiscodeitException(DiscodeitException e) {
+    ErrorCode errorCode = e.getErrorCode();
 
-  @ExceptionHandler(NoSuchElementException.class)
-  public ResponseEntity<String> handleException(NoSuchElementException e) {
-    e.printStackTrace();
-    return ResponseEntity
-        .status(HttpStatus.NOT_FOUND)
-        .body(e.getMessage());
+    ErrorResponse errorResponse = new ErrorResponse(
+        e.getTimestamp(),
+        errorCode.getCode(),
+        errorCode.getMessage(),
+        e.getDetails(),
+        e.getClass().getSimpleName(),
+        HttpStatus.BAD_REQUEST.value()
+    );
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
   }
 
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<String> handleException(Exception e) {
-    e.printStackTrace();
-    return ResponseEntity
-        .status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .body(e.getMessage());
+  public ResponseEntity<ErrorResponse> handleUnexpectedException(Exception e) {
+    ErrorResponse response = new ErrorResponse(
+        Instant.now(),
+        "INTERNAL_ERROR",
+        "알 수 없는 서버 오류가 발생했습니다.",
+        Map.of("message", e.getMessage()),
+        e.getClass().getSimpleName(),
+        HttpStatus.INTERNAL_SERVER_ERROR.value()
+    );
+
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
   }
+
 }
