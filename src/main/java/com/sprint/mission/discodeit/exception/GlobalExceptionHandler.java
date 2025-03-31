172679d04bece09f1a83fc2c5b88cfb4045652bb
you12 <tjf7894@gmail.com>
2025-03-31 10:41:33 +0900

    feat: MethodArgumentNotValidException발생시 GlobalExceptionHandler에서 처리

--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -1,8 +1,14 @@
 package com.sprint.mission.discodeit.exception;
 
+
 import com.sprint.mission.discodeit.dto.error.ErrorResponse;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -26,4 +32,25 @@ public class GlobalExceptionHandler {
     return ResponseEntity.status(status).body(response);
   }
 
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorResponse> habdleValidationErrors(MethodArgumentNotValidException ex) {
+    Map<String, Object> details = new HashMap<>();
+    StringBuilder sb = new StringBuilder();
+    ex.getBindingResult().getAllErrors().forEach(err -> {
+      String fieldName = ((FieldError) err).getField();
+      String errorMessage = err.getDefaultMessage();
+      details.put(fieldName, errorMessage);
+      sb.append(fieldName + ": " + errorMessage).append("; ");
+    });
+
+    ErrorResponse body = new ErrorResponse(
+        Instant.now(),
+        "VALIDATION_FAILED",
+        sb.toString(),
+        details,
+        ex.getClass().getSimpleName(),
+        HttpStatus.BAD_REQUEST.value()
+    );
+    return ResponseEntity.badRequest().body(body);
+  }
 }
