4a4d36f9cd4f5600218185fbc5dd21fc029182b8
yxoni <psy337250@gmail.com>
2025-04-01 15:32:39 +0900

    refactor: H2 데이터베이스와 호환을 고려해 timestamptz -> timestamp로 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE binary_contents
 (
     id           UUID PRIMARY KEY,
-    created_at   TIMESTAMPTZ  NOT NULL,
+    created_at   TIMESTAMP  NOT NULL,
     file_name    VARCHAR(255) NOT NULL,
     size         BIGINT       NOT NULL,
     content_type VARCHAR(100) NOT NULL,
@@ -11,8 +11,8 @@ CREATE TABLE binary_contents
 CREATE TABLE users
 (
     id         UUID PRIMARY KEY,
-    created_at TIMESTAMPTZ  NOT NULL,
-    updated_at TIMESTAMPTZ,
+    created_at TIMESTAMP  NOT NULL,
+    updated_at TIMESTAMP,
     username   VARCHAR(50)  NOT NULL UNIQUE,
     email      VARCHAR(100) NOT NULL UNIQUE,
     password   VARCHAR(255) NOT NULL,
@@ -24,8 +24,8 @@ CREATE TABLE users
 CREATE TABLE channels
 (
     id          UUID PRIMARY KEY,
-    created_at  TIMESTAMPTZ NOT NULL,
-    updated_at  TIMESTAMPTZ,
+    created_at  TIMESTAMP NOT NULL,
+    updated_at  TIMESTAMP,
     name        VARCHAR(100),
     description VARCHAR(500),
     type        VARCHAR(10) NOT NULL CHECK (type IN ('PUBLIC', 'PRIVATE'))
@@ -34,8 +34,8 @@ CREATE TABLE channels
 CREATE TABLE messages
 (
     id         UUID PRIMARY KEY,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
     content    TEXT,
     channel_id UUID        NOT NULL,
     author_id  UUID,
@@ -48,10 +48,10 @@ CREATE TABLE messages
 CREATE TABLE user_statuses
 (
     id             UUID PRIMARY KEY,
-    created_at     TIMESTAMPTZ NOT NULL,
-    updated_at     TIMESTAMPTZ,
+    created_at     TIMESTAMP NOT NULL,
+    updated_at     TIMESTAMP,
     user_id        UUID UNIQUE NOT NULL,
-    last_active_at TIMESTAMPTZ NOT NULL,
+    last_active_at TIMESTAMP NOT NULL,
     CONSTRAINT fk_user_status_user FOREIGN KEY (user_id) REFERENCES
         users (id) ON DELETE CASCADE
 );
@@ -59,11 +59,11 @@ CREATE TABLE user_statuses
 CREATE TABLE read_statuses
 (
     id           UUID PRIMARY KEY,
-    created_at   TIMESTAMPTZ NOT NULL,
-    updated_at   TIMESTAMPTZ,
+    created_at   TIMESTAMP NOT NULL,
+    updated_at   TIMESTAMP,
     user_id      UUID,
     channel_id   UUID,
-    last_read_at TIMESTAMPTZ NOT NULL,
+    last_read_at TIMESTAMP NOT NULL,
     UNIQUE (user_id, channel_id),
     CONSTRAINT fk_read_status_user FOREIGN KEY (user_id) REFERENCES
         users (id) ON DELETE CASCADE,
