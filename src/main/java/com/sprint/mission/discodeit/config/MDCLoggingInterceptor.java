86c45cbe8e52f4910de2518be0ae54043798e0c0
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2025-04-02 11:17:17 +0900

    feat: 심화요구 사항

---
+++ src/main/java/com/sprint/mission/discodeit/config/MDCLoggingInterceptor.java
@@ -0,0 +1,32 @@
+package com.sprint.mission.discodeit.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.UUID;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class MDCLoggingInterceptor implements HandlerInterceptor {
+
+  private static final String REQUEST_ID_HEADER = "Discodeit-RequestId";
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+      Object handler) {
+    String requestId = UUID.randomUUID().toString();
+    MDC.put("requestId", requestId);
+    MDC.put("method", request.getMethod());
+    MDC.put("uri", request.getRequestURI());
+
+    response.setHeader(REQUEST_ID_HEADER, requestId);
+    return true;
+  }
+
+  @Override
+  public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+      Object handler, Exception ex) {
+    MDC.clear();
+  }
+}
