42ad83053252509a08fa7fa4f563508cbaded9f6
hyojkim2 <skql2548@gmail.com>
2025-04-03 22:36:05 +0900

    :pencil2: UserUpdateDTO 필드명 수정

--- src/main/java/com/sprint/mission/discodeit/dto/user/UserUpdateDTO.java
+++ src/main/java/com/sprint/mission/discodeit/dto/user/UserUpdateDTO.java
@@ -1,20 +1,18 @@
 package com.sprint.mission.discodeit.dto.user;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import java.util.UUID;
 
 public record UserUpdateDTO
     (
         @Size(min = 2, max = 10, message = "이름은 2자 이상, 10자 이하로 입력해야 합니다.")
-        String name,
+        String newName,
 
         @Size(min = 4, max = 16, message = "비밀번호는 8자 이상, 16 이하로 입력해야 합니다.")
-        String password,
+        String newPassword,
 
         @Email(message = "이메일 형식이 맞아야합니다.")
-        String email,
+        String newEmail,
         String newFilePAth
     ) {
 
