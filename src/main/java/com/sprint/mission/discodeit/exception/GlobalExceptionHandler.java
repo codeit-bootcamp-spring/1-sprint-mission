37ed7f6afac8454fa621e06c980bd7d16a77db16
yxoni <psy337250@gmail.com>
2025-04-06 05:07:03 +0900

    feat: MethodArgumentNotValidException 전역 예외 처리 추가

--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -1,9 +1,17 @@
 package com.sprint.mission.discodeit.exception;
 
 import com.sprint.mission.discodeit.dto.exception.ErrorResponse;
+import java.time.Instant;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,5 +23,29 @@ public class GlobalExceptionHandler {
         return ResponseEntity
             .status(e.getErrorCode().getHttpStatus())
             .body(errorResponse);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
+        Map<String, Object> details = e.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .collect(Collectors.toMap(
+                FieldError::getField,
+                fieldError -> Optional.ofNullable(fieldError.getDefaultMessage()).orElse("잘못된 값입니다."),
+                (existing, replacement) -> existing
+            ));
+
+        ErrorResponse errorResponse = new ErrorResponse(
+            Instant.now(),
+            "VALIDATION_001",
+            "유효성 검증에 실패했습니다.",
+            details,
+            e.getClass().getSimpleName(),
+            HttpStatus.BAD_REQUEST.value()
+        );
+
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 }
