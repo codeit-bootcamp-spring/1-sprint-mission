9609d04b6939ce2ed9c488e6990d266e4fd25a32
hyojkim2 <skql2548@gmail.com>
2025-04-06 22:31:45 +0900

    :sparkles: 수정

--- src/main/java/com/sprint/mission/discodeit/controller/UserController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -31,8 +31,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
-
-
+  
   private final UserService userService;
 
   // 사용자 등록
