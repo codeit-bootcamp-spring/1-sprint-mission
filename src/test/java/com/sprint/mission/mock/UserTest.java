48a7daf0cdca4de5914b821528baa500281a9474
이요한 <icb1696@naver.com>
2025-04-06 12:15:58 +0900

    merge 전 커밋

--- src/test/java/com/sprint/mission/mock/UserTest.java
+++ src/test/java/com/sprint/mission/mock/UserTest.java
@@ -26,20 +26,25 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserTest {
 
     private static final Logger log = LoggerFactory.getLogger(UserTest.class);
+
     @Mock
     private BinaryService binaryService;
+
     @Mock
     private UserRepository userRepository;
 
@@ -57,24 +62,6 @@ public class UserTest {
     @InjectMocks
     private JCFUserService userService;
 
-    //@Slf4j
-    //public record BinaryContentDtoForCreate(
-    //
-    //        @Schema(example = "zessy")
-    //        String fileName,
-    //        String contentType,
-    //        Long size,
-    //        byte[] bytes) {
-
-
-    //
-//    public BinaryContent(String fileName, Long size, String contentType) {
-//        this.fileName = fileName;
-//        this.size = size;
-//        this.contentType = contentType;
-//    }
-
-    //MultipartFile file = Mockito.mock(MultipartFile.class, withSettings().useConstructor("testFile", "image/png", 123L, new byte[0]));
     @Test
     @DisplayName("UserService.create() 시 관련된 의존성을 Mocking하여 실제 호출하는지 테스트")
     void createTest(){
@@ -82,27 +69,25 @@ public class UserTest {
         // given
         // 1. 필요한 의존성과 격리시키기 : binary 서비스 + user레포지토리 + userstatus 서비스
         stub_binaryService_userRepository_userStatusService_ForCreate();
-        MockMultipartFile mockMultipartFile = new MockMultipartFile(
-                "file name", // name
-                "originalFileName", // originalFilename
-                "image/png", // contentType
-                "fake byte".getBytes() // content
-        );
+        MockMultipartFile mockMultipartFile = getMockMultipartFile();
 
         // when
         UserDtoForCreate dto1 = new UserDtoForCreate("테스트 유저 2", "testPassword", "testEmail");
-        User user = userService.create(dto1, mockMultipartFile);
-        log.info("user.getProfile() : {}", user.getProfile());  // user.getProfile() : BinaryContent(fileName=testFile, size=123, contentType=image/png)
+        User createdUser = userService.create(dto1, mockMultipartFile);
+        log.info("user.getProfile() : {}", createdUser.getProfile());  // user.getProfile() : BinaryContent(fileName=testFile, size=123, contentType=image/png)
 
-        // then
+        // then1 : 연관된 메서드들이 호출됐는지
         verify(binaryContentMapper, times(1)).convertFileToBinaryContentDto(mockMultipartFile);
-
         verify(binaryService, times(1)).create(any());  // binaryService.create 호출 확인인데 stub에 설정해놨기에 호출됨
-        verify(binaryContentMapper, times(1)).convertFileToBinaryContentDto(any());
-        verify(userRepository, atMost(3)).save(any(User.class));
+        verify(userRepository, atMost(1)).save(any(User.class));
         verify(userStatusService, atLeast(1)).create(any(User.class));
 
-
+        // then2 : 생성된 user가 올바르게 세팅 됐는지
+        assertThat(createdUser.getCreatedAt()).isNotNull();
+        assertThat(createdUser.getId()).isNotNull();
+        assertThat(createdUser.getUsername()).isEqualTo(dto1.username());
+        assertThat(createdUser.getPassword()).isEqualTo(dto1.password());
+        assertThat(createdUser.getEmail()).isEqualTo(dto1.email());
     }
 
 
@@ -112,34 +97,47 @@ public class UserTest {
         // given
         // 1. 필요한 의존성과 격리시키기 : binary 서비스 + user레포지토리 + userstatus 서비스
         stub_binaryService_userRepository_userStatusService_ForCreate();
+        String duplicatedName = "testUser";
 
         List<User> userList = new ArrayList<>();
-        userList.add(new User("testUser", "testPassword", "testEmail", null));
+        userList.add(new User(duplicatedName, "testPassword", "testEmail", null));
         when(userRepository.findAll()).thenReturn(userList);
 
         // when
         // 2. 이름 및 이메일 중복 테스트
-        UserDtoForCreate userDto = new UserDtoForCreate("testUser", "testPassword", "testEmail");
+        UserDtoForCreate userDto = new UserDtoForCreate(duplicatedName, "testPassword", "testEmail");
 
-        Assertions.assertThatThrownBy(() -> userService.create(userDto, null))
+        assertThatThrownBy(() -> userService.create(userDto, getMockMultipartFile()))
                 .isInstanceOf(CustomException.class);
     }
 
 
     private void stub_binaryService_userRepository_userStatusService_ForCreate() {
+
+        //lenient() : 이 stub은 테스트 중 반드시 사용되지 않아도 된다”는 의미를 부여 (이렇게 하지 않으면 오류 발생)
         BinaryContent binaryContent = new BinaryContent("testFile", 123L, "image/png");
-        when(binaryService.create(any())).thenReturn(binaryContent);
-        when(userRepository.save(any(User.class))).thenAnswer(invocation -> {
+        lenient().when(binaryService.create(any())).thenReturn(binaryContent);
+        lenient().when(userRepository.save(any(User.class))).thenAnswer(invocation -> {
             User user = invocation.getArgument(0);
-            user.setId(UUID.randomUUID());
+            ReflectionTestUtils.setField(user, "id", UUID.randomUUID());
+            ReflectionTestUtils.setField(user, "createdAt", Instant.now());
+            log.info("리플렉션을 통한 user의 id : {}", user.getId()); //리플렉션을 통한 user의 id : 092997ef-591f-4eb2-ac6d-4ca3a8c76457
+            log.info("리플렉션을 통한 user의 createdAt : {}", user.getCreatedAt());
             return user;
         });
-        when(userStatusService.create(any())).thenAnswer(invocationOnMock -> {
+        lenient().when(userStatusService.create(any())).thenAnswer(invocationOnMock -> {
             return new UserStatus(invocationOnMock.getArgument(0));
         });
     }
 
-
+    private MockMultipartFile getMockMultipartFile() {
+        return new MockMultipartFile(
+                "file name", // name
+                "originalFileName", // originalFilename
+                "image/png", // contentType
+                "fake byte".getBytes() // content
+        );
+    }
 }
 //public record UserDtoForCreate(
 //        String username,
