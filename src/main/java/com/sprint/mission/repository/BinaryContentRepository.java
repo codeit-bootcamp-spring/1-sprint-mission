dc3e99ff8ad9e431cc83cdd13b0324a875caef1e
이요한 <icb1696@naver.com>
2025-04-19 12:38:12 +0900

    d

--- src/main/java/com/sprint/mission/repository/BinaryContentRepository.java
+++ src/main/java/com/sprint/mission/repository/BinaryContentRepository.java
@@ -1,19 +1,19 @@
 package com.sprint.mission.repository;
 
 import com.sprint.mission.entity.addOn.BinaryContent;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-public interface BinaryContentRepository extends JpaRepository<BinaryContent, UUID> {
-}
+public interface BinaryContentRepository {
+    BinaryContent save(BinaryContent binaryContent);
 
-//   BinaryContent save(BinaryContent binaryContent);
-//
-//    Optional<BinaryContent> findById(UUID id);
-//
-//    List<BinaryContent> findAllByIdIn(List<UUID> idList);
-//
-//    void delete(UUID id);
-//
-//    boolean existsById(UUID id);
+    Optional<BinaryContent> findById(UUID id);
+
+    List<BinaryContent> findAllByIdIn(List<UUID> idList);
+
+    void delete(UUID id);
+
+    boolean existsById(UUID id);
+}
