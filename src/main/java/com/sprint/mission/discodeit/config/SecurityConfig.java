0952b494b02befde4c0a54b0106f9ecc37369dfb
kyungrin Kim <0123jade0123@gmail.com>
2025-05-19 18:34:50 +0900

    feat: Spring Security 환경 설정 - SecurityConfig 설정 : SecurityFilterChain Bean 등록시 LoginFilter 제외 - application.yml : Spring Security 모듈의 로깅 레벨 trace 로 설정

---
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.sprint.mission.discodeit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain chain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers(
+                "/",
+                "/swagger-ui/**",
+                "/v3/api-docs/**",
+                "/actuator/**").permitAll()
+            .requestMatchers("/api/**").authenticated()
+            .anyRequest().authenticated())
+        .httpBasic(Customizer.withDefaults())
+        .logout(AbstractHttpConfigurer::disable);
+    return http.build();
+  }
+}
