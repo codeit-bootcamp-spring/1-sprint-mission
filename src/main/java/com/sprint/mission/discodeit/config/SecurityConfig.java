8ccdec1f8d582f2a9339a622b1a8f1eb1aeb73b1
yerimYeon <99879845+yinneu@users.noreply.github.com>
2025-05-20 17:07:14 +0900

    chore: csrf 토큰 발급 설정 및 api 구현

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -5,15 +5,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    /**
+     * 1. discodeit은 로그아웃 페이지를 CSR로 처리하므로 LogoutFilter 제외
+     * <p>
+     * 2. HttpOnly를 false로 설정하여 JavaScript에서 쿠키에 접근 가능하게 함
+     */
     @Bean
     SecurityFilterChain chain(HttpSecurity http) throws Exception {
         http
+            .csrf(csrf -> csrf
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            )
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/auth/csrf-token").permitAll()
                 .requestMatchers("/api/**").authenticated()
                 .anyRequest().permitAll())
             .logout(logout -> logout.logoutUrl("/disabled"));
--- src/main/java/com/sprint/mission/discodeit/controller/api/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/api/AuthController.java
@@ -9,6 +9,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +31,11 @@ public class AuthController implements AuthApiDocs {
 
         log.info("POST /api/login - login attempt for user: {}", userRequestLogin.getUsername());
         return ResponseEntity.ok(authService.login(userRequestLogin));
+    }
+
+    @GetMapping("/csrf-token")
+    public ResponseEntity<CsrfToken> getCsrfToken(CsrfToken csrfToken) {
+        // spring security가 자동으로 주입
+        return ResponseEntity.ok(csrfToken);
     }
 }
