bc44f1b91af324f524a604822ffc4d8bbfafe7d6
mingi96 <minnet1030@naver.com>
2025-05-22 14:36:24 +0900

    세션을 활용한 현재 사용자 정보 조회

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -2,14 +2,14 @@ package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.controller.api.AuthApi;
 import com.sprint.mission.discodeit.dto.data.UserDto;
-import com.sprint.mission.discodeit.dto.request.LoginRequest;
+import com.sprint.mission.discodeit.mapper.UserMapper;
+import com.sprint.mission.discodeit.security.CustomUserDetails;
 import com.sprint.mission.discodeit.service.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,12 +20,18 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController implements AuthApi {
 
   private final AuthService authService;
-
-
+  private final UserMapper userMapper;
 
   @GetMapping("/api/auth/csrf-token")
   public ResponseEntity<CsrfToken> getCsrfToken(HttpServletRequest request) {
     CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
     return ResponseEntity.ok(csrfToken);
   }
+
+  @GetMapping("/me")
+  public ResponseEntity<UserDto> getCurrentUser(@AuthenticationPrincipal CustomUserDetails userDetails) {
+    log.debug("현재 로그인된 사용자 조회 요청");
+    return ResponseEntity.ok(userMapper.toDto(userDetails.getUser()));
+  }
+
 }
