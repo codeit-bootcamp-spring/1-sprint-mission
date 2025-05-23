be8b9343cb6f36854242d4f9aa252cd41f1c8d6f
janghoosa <janghoosa@gmail.com>
2025-05-23 09:43:22 +0900

    feat: 인증까지 구현

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -1,29 +1,46 @@
 package com.sprint.mission.discodeit.config;
 
+import com.sprint.mission.discodeit.filter.LoginAuthenticationFilter;
+import com.sprint.mission.discodeit.filter.LogoutFilter;
+import com.sprint.mission.discodeit.provider.LoginAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+  private final LoginAuthenticationProvider loginAuthenticationProvider;
+
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
-      Exception {
-    http
+  public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+    return http.getSharedObject(AuthenticationManagerBuilder.class)
+        .authenticationProvider(loginAuthenticationProvider)
+        .build();
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(
+      HttpSecurity http,
+      AuthenticationManager authenticationManager
+  ) throws Exception {
+    LoginAuthenticationFilter loginFilter = new LoginAuthenticationFilter(authenticationManager);
+
+    http.csrf(csrf -> csrf
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
         .logout(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(authorize -> authorize
-            // 인증 제외 경로: Swagger, 정적 리소스, actuator 등
             .requestMatchers(
                 "/swagger-ui/**",
                 "/v3/api-docs/**",
@@ -32,42 +49,23 @@ public class SecurityConfig {
                 "/actuator/**",
                 "/api/auth/csrf-token",
                 "/api/auth/login",
+                "/api/auth/logout",
                 "/css/**",
                 "/js/**",
                 "/images/**",
                 "/",
-                "/favicon.ico"
-            )
+                "/favicon.ico")
             .permitAll()
-            .requestMatchers(
-                HttpMethod.POST,
-                "/api/users")
+            .requestMatchers(HttpMethod.POST, "/api/users")
             .permitAll()
 
-            // 나머지 경로는 모두 인증 요구
             .anyRequest()
-            .authenticated()
-        )
-        .formLogin(Customizer.withDefaults()) // 기본 form 로그인 사용
-        .httpBasic(Customizer.withDefaults()) // HTTP Basic 인증도 활성화
-        .csrf(AbstractHttpConfigurer::disable); // CSRF는 일단 개발용으로 비활성화
-
+            .authenticated())
+        .addFilterBefore(new LogoutFilter(), UsernamePasswordAuthenticationFilter.class)
+        .addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class)
+        .authenticationProvider(loginAuthenticationProvider)
+        .formLogin(AbstractHttpConfigurer::disable)
+        .httpBasic(AbstractHttpConfigurer::disable);
     return http.build();
   }
-
-  @Bean
-  UserDetailsService uds(PasswordEncoder enc) {
-    UserDetails u = User.withUsername("api-user")
-        .password(enc.encode("p@ssw0rd")) //BCrypt 암호화
-        .roles("USER")
-        .build();
-    return new InMemoryUserDetailsManager(u);
-  }
-
-  @Bean
-  PasswordEncoder enc() {
-    return new BCryptPasswordEncoder();
-  }
-
-
 }
