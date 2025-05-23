2a12473192d6f03a752db4ab2e06610bcd12ca68
Oince <hky0105@naver.com>
2025-05-23 14:21:32 +0900

    feat: passwordEncoder를 사용해 비밀번호 해시

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -5,9 +5,12 @@ import static org.springframework.security.config.Customizer.withDefaults;
 import com.sprint.mission.discodeit.filter.CsrfCookieFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -24,7 +27,8 @@ public class SecurityConfig {
     return httpSecurity
         .authorizeHttpRequests(requests -> requests
             .requestMatchers("/api/**").authenticated()
-            .requestMatchers("/api/auth/csrf-token", "api/auth/login").permitAll()
+            .requestMatchers("/api/auth/csrf-token", "/api/auth/login").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
             .anyRequest().permitAll())
         .csrf(csrfConfig -> csrfConfig
             .csrfTokenRepository(cookieCsrfTokenRepository)
@@ -37,5 +41,10 @@ public class SecurityConfig {
         .formLogin(AbstractHttpConfigurer::disable)
         .httpBasic(withDefaults())
         .build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
   }
 }
