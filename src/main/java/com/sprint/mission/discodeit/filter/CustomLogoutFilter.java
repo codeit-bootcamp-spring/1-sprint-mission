3297d9f6cba452d126b25239bf135216cf6aca17
yudility <yudility@gmail.com>
2025-05-23 16:42:03 +0900

    fix: 로그아웃 시 사용자의 세션 모두 삭제되도록 수정

--- src/main/java/com/sprint/mission/discodeit/filter/CustomLogoutFilter.java
+++ src/main/java/com/sprint/mission/discodeit/filter/CustomLogoutFilter.java
@@ -6,13 +6,23 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.session.SessionInformation;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 // 로그아웃 필터 구현
+@Component
+@RequiredArgsConstructor
 public class CustomLogoutFilter extends OncePerRequestFilter {
+
+  private final SessionRegistry sessionRegistry;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request,
@@ -21,6 +31,20 @@ public class CustomLogoutFilter extends OncePerRequestFilter {
 
     if ("/api/auth/logout".equals(request.getRequestURI()) &&
         "POST".equalsIgnoreCase(request.getMethod())) {
+
+      Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+      if (authentication != null && authentication.isAuthenticated()) {
+        Object principal = authentication.getPrincipal();
+
+        // 해당 사용자의 모든 세션 정보 가져오기
+        List<SessionInformation> sessions = sessionRegistry.getAllSessions(principal, false);
+
+        // 모든 세션 만료 및 제거
+        for (SessionInformation sessionInfo : sessions) {
+          sessionInfo.expireNow();
+          sessionRegistry.removeSessionInformation(sessionInfo.getSessionId());
+        }
+      }
 
       // 로그아웃
       SecurityContextHolder.clearContext(); //SecurityContext 초기화
