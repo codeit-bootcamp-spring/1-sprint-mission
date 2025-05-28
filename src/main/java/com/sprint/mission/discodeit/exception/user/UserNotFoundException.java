66f1c19ed05a389a8a93cf2a7acfb26084796cb5
yudility <yudility@gmail.com>
2025-05-28 18:41:28 +0900

    refactor: User 예외 생성 방식 추가

--- src/main/java/com/sprint/mission/discodeit/exception/user/UserNotFoundException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/user/UserNotFoundException.java
@@ -7,4 +7,13 @@ public class UserNotFoundException extends UserException {
   public UserNotFoundException(ErrorCode errorCode) {
     super(errorCode);
   }
+
+  public static UserNotFoundException withUsername(String username) {
+    UserNotFoundException exception = new UserNotFoundException(ErrorCode.USER_NOT_FOUND);
+    throw new UserNotFoundException(ErrorCode.USER_NOT_FOUND);
+  }
+
+  //    InterestNotFoundException exception = new InterestNotFoundException();
+  //    exception.addDetail("interestId", interestId);
+  //    return exception;
 }
