54ce3ac7bfb1e2f8316db510a492c43d313666f9
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-06-04 17:54:25 +0900

    feat: 비동기 처리 설정

--- src/main/java/com/sprint/mission/discodeit/config/AsyncConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/AsyncConfig.java
@@ -3,17 +3,36 @@ package com.sprint.mission.discodeit.config;
 import java.util.concurrent.Executor;
 
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+@Configuration
+@EnableAsync
 public class AsyncConfig implements AsyncConfigurer {
 
 	@Override
 	public Executor getAsyncExecutor() {
-		return AsyncConfigurer.super.getAsyncExecutor();
+		ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+		threadPoolTaskExecutor.setCorePoolSize(20);
+		threadPoolTaskExecutor.setMaxPoolSize(50);
+		threadPoolTaskExecutor.setQueueCapacity(1000);
+		threadPoolTaskExecutor.setThreadNamePrefix("io-AsyncExecutor-");
+		threadPoolTaskExecutor.setKeepAliveSeconds(60);
+		threadPoolTaskExecutor.initialize();
+		return threadPoolTaskExecutor;
 	}
 
 	@Override
 	public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
-		return AsyncConfigurer.super.getAsyncUncaughtExceptionHandler();
+		return (ex, method, params) -> {
+			// 예외 처리 로직을 여기에 작성합니다.
+			System.err.println("Async method threw an exception: " + ex.getMessage());
+			System.err.println("Method: " + method.getName());
+			for (Object param : params) {
+				System.err.println("Param: " + param);
+			}
+		};
 	}
 }
