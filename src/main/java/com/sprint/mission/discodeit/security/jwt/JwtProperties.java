662182a9c3c258e910397382eded5c01f65190d5
yerimYeon <99879845+yinneu@users.noreply.github.com>
2025-06-05 16:45:59 +0900

    refacor(mentor): JwtProperties 불변성 보장

--- src/main/java/com/sprint/mission/discodeit/security/jwt/JwtProperties.java
+++ src/main/java/com/sprint/mission/discodeit/security/jwt/JwtProperties.java
@@ -4,36 +4,36 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "jwt")
 @Getter
-@Setter
 @Validated
+@RequiredArgsConstructor
 public class JwtProperties {
 
     @NotBlank
-    private String issuer = "discodeit-api"; // iss=발급자
+    private final String issuer; // iss=발급자
 
     @NotBlank
     @Size(min = 32, message = "JWT secret must be at least 32 characters")
-    private String secret; // 토큰 서명 비밀 키, 필수값, 32자 이상
+    private final String secret; // 토큰 서명 비밀 키, 필수값, 32자 이상
 
     @Valid
-    private TokenConfig accessToken = new TokenConfig();
+    private final TokenConfig accessToken;
 
     @Valid
-    private TokenConfig refreshToken = new TokenConfig();
+    private final TokenConfig refreshToken;
 
-    // Token 에 공통 적용될 구성
-    @Data
+    @Getter
+    @Validated
+    @RequiredArgsConstructor
     public static class TokenConfig {
 
         @Min(value = 60, message = "Token validity must be at least 60 seconds")
-        private long validitySeconds = 900; // 15분
+        private final long validitySeconds;
     }
 }
