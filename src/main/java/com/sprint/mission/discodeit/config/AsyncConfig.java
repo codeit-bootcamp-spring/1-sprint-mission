00c43b4b8934b380dc2ba068092f28e81c6ff95d
TaeHyoun Kim <kth9903@gmail.com>
2025-06-06 14:45:47 +0900

    feat: 비동기 처리 및 Spring Retry 설정

---
+++ src/main/java/com/sprint/mission/discodeit/config/AsyncConfig.java
@@ -0,0 +1,27 @@
+package com.sprint.mission.discodeit.config;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
+
+@Configuration
+@EnableAsync
+@EnableRetry
+public class AsyncConfig {
+
+    public Executor fileUploadExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("FileUpload-");
+        executor.setRejectedExecutionHandler(new CallerRunsPolicy());
+        executor.initialize();
+
+        return new DelegatingSecurityContextAsyncTaskExecutor(executor);
+    }
+}
