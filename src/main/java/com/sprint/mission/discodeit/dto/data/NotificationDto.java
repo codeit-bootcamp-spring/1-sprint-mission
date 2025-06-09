9599dbc61024e5340a3deb4c94c4ac309317b6d3
TaeHyoun Kim <kth9903@gmail.com>
2025-06-09 14:17:26 +0900

    fix: 필드 타입 수정

--- src/main/java/com/sprint/mission/discodeit/dto/data/NotificationDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/data/NotificationDto.java
@@ -1,14 +1,13 @@
 package com.sprint.mission.discodeit.dto.data;
 
 import com.sprint.mission.discodeit.entity.NotificationType;
-import com.sprint.mission.discodeit.entity.User;
 import java.time.Instant;
 import java.util.UUID;
 
 public record NotificationDto(
     UUID id,
     Instant createdAt,
-    User receiver,
+    UUID receiverId,
     String title,
     String content,
     NotificationType type,
