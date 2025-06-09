78fb2dd864024d89291e7a10d274500320f4a98d
TaeHyoun Kim <kth9903@gmail.com>
2025-06-09 17:36:05 +0900

    feat:  BasicNotificationService 구현

---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicNotificationService.java
@@ -0,0 +1,63 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.dto.data.NotificationDto;
+import com.sprint.mission.discodeit.entity.Notification;
+import com.sprint.mission.discodeit.exception.DiscodeitException;
+import com.sprint.mission.discodeit.exception.ErrorCode;
+import com.sprint.mission.discodeit.mapper.NotificationMapper;
+import com.sprint.mission.discodeit.repository.NotificationRepository;
+import com.sprint.mission.discodeit.service.NotificationService;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class BasicNotificationService implements NotificationService {
+
+    private final NotificationRepository notificationRepository;
+    private final NotificationMapper notificationMapper;
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<NotificationDto> findAllByReceiverId(UUID receiverId) {
+        log.debug("사용자별 알림 목록 조회 시작: receiverId={}", receiverId);
+        List<NotificationDto> notifications = notificationRepository
+            .findAllByReceiverIdOrderByCreatedAtDesc(receiverId)
+            .stream()
+            .map(notificationMapper::toDto)
+            .toList();
+
+        log.info("사용자별 알림 목록 조회 완료: receiverId={}, 알림 수={}", receiverId, notifications.size());
+        return notifications;
+    }
+
+    @Transactional
+    @Override
+    public void delete(UUID notificationId, UUID requesterId) {
+        log.debug("알림 삭제 시작: notificationId={}, requesterId={}", notificationId, requesterId);
+
+        Notification notification = notificationRepository.findByIdWithReceiver(notificationId)
+            .orElseThrow(() -> {
+                DiscodeitException exception = new DiscodeitException(
+                    ErrorCode.NOTIFICATION_NOT_FOUND);
+                exception.addDetail("notificationId", notificationId);
+                return exception;
+            });
+
+        if (!notification.getReceiver().getId().equals(requesterId)) {
+            DiscodeitException exception = new DiscodeitException(ErrorCode.INVALID_REQUEST);
+            exception.addDetail("reason", "Cannot delete other user's notification");
+            exception.addDetail("notificationId", notificationId);
+            exception.addDetail("receiverId", notification.getReceiver().getId());
+            throw exception;
+        }
+
+        notificationRepository.delete(notification);
+        log.info("알림 삭제 완료: notifcationId={}", notificationId);
+    }
+}
