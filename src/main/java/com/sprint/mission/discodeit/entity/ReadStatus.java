d2d0c101356257e704c51f1b107ed269cde48650
TaeHyoun Kim <kth9903@gmail.com>
2025-06-09 13:41:30 +0900

    refactor: notificationEnabled Boolean 필드 추가 - 채널 타입에 따른 기본값 설정 로직 구현 - 새로운 생성자와 업데이트 메서드 추가

--- src/main/java/com/sprint/mission/discodeit/entity/ReadStatus.java
+++ src/main/java/com/sprint/mission/discodeit/entity/ReadStatus.java
@@ -24,24 +24,48 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ReadStatus extends BaseUpdatableEntity {
 
-  @ManyToOne(fetch = FetchType.LAZY, optional = false)
-  @JoinColumn(name = "user_id", columnDefinition = "uuid")
-  private User user;
-  @ManyToOne(fetch = FetchType.LAZY, optional = false)
-  @JoinColumn(name = "channel_id", columnDefinition = "uuid")
-  private Channel channel;
-  @Column(columnDefinition = "timestamp with time zone", nullable = false)
-  private Instant lastReadAt;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", columnDefinition = "uuid")
+    private User user;
 
-  public ReadStatus(User user, Channel channel, Instant lastReadAt) {
-    this.user = user;
-    this.channel = channel;
-    this.lastReadAt = lastReadAt;
-  }
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "channel_id", columnDefinition = "uuid")
+    private Channel channel;
 
-  public void update(Instant newLastReadAt) {
-    if (newLastReadAt != null && !newLastReadAt.equals(this.lastReadAt)) {
-      this.lastReadAt = newLastReadAt;
+    @Column(columnDefinition = "timestamp with time zone", nullable = false)
+    private Instant lastReadAt;
+
+    @Column(nullable = false)
+    private Boolean notificationEnabled;
+
+    public ReadStatus(User user, Channel channel, Instant lastReadAt) {
+        this.user = user;
+        this.channel = channel;
+        this.lastReadAt = lastReadAt;
+        this.notificationEnabled = channel.getType() == ChannelType.PRIVATE;
     }
-  }
+
+    public ReadStatus(User user, Channel channel, Instant lastReadAt, Boolean notificationEnabled) {
+        this.user = user;
+        this.channel = channel;
+        this.lastReadAt = lastReadAt;
+        this.notificationEnabled = notificationEnabled != null ? notificationEnabled :
+            (channel.getType() == ChannelType.PRIVATE);
+    }
+
+    public void update(Instant newLastReadAt) {
+        if (newLastReadAt != null && !newLastReadAt.equals(this.lastReadAt)) {
+            this.lastReadAt = newLastReadAt;
+        }
+    }
+
+    public void update(Instant newLastReadAt, Boolean newNotificationEnabled) {
+        if (newLastReadAt != null && !newLastReadAt.equals(this.lastReadAt)) {
+            this.lastReadAt = newLastReadAt;
+        }
+        if (newNotificationEnabled != null && !newNotificationEnabled.equals(
+            this.notificationEnabled)) {
+            this.notificationEnabled = newNotificationEnabled;
+        }
+    }
 }
