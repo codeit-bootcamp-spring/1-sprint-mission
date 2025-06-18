e3b1a7a2e3d7fe0ed0e4b99c90860858300aba9d
yudility <yudility@gmail.com>
2025-06-18 14:44:15 +0900

    feat: WebSocket Config 구현

---
+++ src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
@@ -0,0 +1,35 @@
+package com.sprint.mission.discodeit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker // STOMP 프로토콜을 사용하는 메시지 브로커를 활성화
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+  // 메시지 브로커 관련 설정
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry registry) {
+    // 클라이언트가 서버로 메시지를 보낼 때 사용할 prefix
+    // 클라이언트가 /pub/messages 로 메시지를 보내면
+    // 서버 측 @MessageMapping("/messages") 로 라우팅 됨
+    registry.setApplicationDestinationPrefixes("/pub");
+
+    // 클라이언트가 구독할 수 있는 prefix
+    // /sub/channels.{channelId}.messages 형태로 구독
+    registry.enableSimpleBroker("/sub");
+  }
+
+
+  // 클라이언트가 WebSocket 연결을 맺기 위한 엔드포인트 정의
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    // WebSocket 연결 경로
+    registry.addEndpoint("/ws")
+        .setAllowedOriginPatterns("*")
+        .withSockJS(); // SockJS fallback 지원
+  }
+}
