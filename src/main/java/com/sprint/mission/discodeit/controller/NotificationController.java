803f2cd0a40f7ddb7990f0175a2bcd544f65dbed
kyungrin Kim <0123jade0123@gmail.com>
2025-06-18 10:45:17 +0900

    fix: 알림 기능이 작동하지 않는 문제 해결 - @AuthenticationPrincipal 어노테이션을 통해 받아오는 객체 변경

--- src/main/java/com/sprint/mission/discodeit/controller/NotificationController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/NotificationController.java
@@ -4,8 +4,8 @@ import com.sprint.mission.discodeit.dto.NotificationDto;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.exception.user.UserNotFoundException;
 import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
 import com.sprint.mission.discodeit.service.basic.NotificationService;
-import java.nio.file.attribute.UserPrincipal;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -27,21 +27,15 @@ public class NotificationController {
 
   private final NotificationService notificationService;
   private final UserRepository userRepository;
-
-  /**
-   * 알림 조회, 확인(삭제)은 요청자 본인의 알림에 대해서만 수행할 수 있습니다.
-   * <p>
-   * 알림 조회 및 삭제 시 현재 요청의 인증 정보로 요청자를 식별합니다.
-   **/
+  
   @GetMapping
   public ResponseEntity<List<NotificationDto>> getNotifications(
-      @AuthenticationPrincipal UserPrincipal userPrincipal
+      @AuthenticationPrincipal DiscodeitUserDetails principal
   ) {
-    log.info("userPrincipal={}", userPrincipal);
-    String username = userPrincipal.getName();
+    UUID receiverId = principal.getUserDto().id();
 
-    User user = userRepository.findByUsername(username)
-        .orElseThrow(() -> new UserNotFoundException(Map.of("username", username)));
+    User user = userRepository.findById(receiverId)
+        .orElseThrow(() -> new UserNotFoundException(Map.of("receiverId", receiverId)));
     log.info("알림 조회 시도");
     List<NotificationDto> notificationDtos = notificationService.find(user.getId());
 
@@ -53,11 +47,11 @@ public class NotificationController {
   @DeleteMapping("/{notificationId}")
   public ResponseEntity<Void> delete(
       @PathVariable UUID notificationId,
-      @AuthenticationPrincipal UserPrincipal userPrincipal // 우선 이렇게... 내가 구현한 게 아니라서 어떻게 되는지 모름
+      @AuthenticationPrincipal DiscodeitUserDetails principal
   ) {
-    String username = userPrincipal.getName();
-    User user = userRepository.findByUsername(username)
-        .orElseThrow(() -> new UserNotFoundException(Map.of("username", username)));
+    UUID receiverId = principal.getUserDto().id();
+    User user = userRepository.findById(receiverId)
+        .orElseThrow(() -> new UserNotFoundException(Map.of("receiverId", receiverId)));
 
     log.info("알림 삭제(확인) 시도");
     notificationService.delete(notificationId, user.getId());
