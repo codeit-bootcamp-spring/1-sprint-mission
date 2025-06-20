42b2524d6eddea9f8a0c80823d50f1937ccd5f82
Oince <hky0105@naver.com>
2025-06-20 15:31:45 +0900

    feat: redis 적용

--- src/main/java/com/sprint/mission/discodeit/config/CacheConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/CacheConfig.java
@@ -1,26 +1,52 @@
 package com.sprint.mission.discodeit.config;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import java.time.Duration;
-import org.springframework.cache.CacheManager;
+import java.util.Set;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 @Configuration
 @EnableCaching
+@RequiredArgsConstructor
 public class CacheConfig {
 
-  @Bean
-  public CacheManager cacheManager() {
-    CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager(CacheName.ALL);
-    caffeineCacheManager.setCaffeine(
-        Caffeine.newBuilder()
-            .maximumSize(100)
-            .expireAfterAccess(Duration.ofMinutes(5))
-            .recordStats()
+  private final ObjectMapper objectMapper;
+
+  private RedisCacheConfiguration redisCacheConfiguration() {
+    ObjectMapper redisObjectMapper = objectMapper.copy();
+    redisObjectMapper.activateDefaultTyping(
+        LaissezFaireSubTypeValidator.instance,
+        DefaultTyping.EVERYTHING,
+        As.PROPERTY
     );
-    return caffeineCacheManager;
+
+    return RedisCacheConfiguration.defaultCacheConfig()
+        .serializeValuesWith(
+            RedisSerializationContext.SerializationPair.fromSerializer(
+                new GenericJackson2JsonRedisSerializer(redisObjectMapper)
+            )
+        )
+        .prefixCacheNameWith("discodeit:")
+        .entryTtl(Duration.ofSeconds(600))
+        .disableCachingNullValues();
+  }
+
+  @Bean
+  public RedisCacheManagerBuilderCustomizer redisCustomizer() {
+    return builder -> builder
+        .cacheDefaults(redisCacheConfiguration())
+        .initialCacheNames(Set.of(CacheName.ALL))
+        .enableStatistics()
+        .build();
   }
 }
